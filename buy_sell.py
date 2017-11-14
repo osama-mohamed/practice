47f3f010396fc488af310f201cb0148bcc76cca6
OSAMA.MOHAMED <om1234@fayoum.edu.eg>
2017-11-14 22:32:11 +0200

    more edit on slider images

--- buy_sell.py
+++ buy_sell.py
@@ -108,6 +108,12 @@ def favicon():
 @app.route('/', methods=['post', 'get'])
 def home():
     cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM slider_products LIMIT 1")
+    slider_products_first = cur.fetchall()
+    cur.execute("SELECT * FROM slider_products LIMIT 1 OFFSET 1")
+    slider_products_second = cur.fetchall()
+    cur.execute("SELECT * FROM slider_products ORDER BY id DESC LIMIT 1")
+    slider_products_third = cur.fetchall()
     cur.execute("SELECT * FROM products ORDER BY id DESC LIMIT 6;")
     latest_products = cur.fetchall()
     cur.execute("SELECT * FROM categories")
@@ -115,7 +121,7 @@ def home():
     # cur.execute("SELECT * FROM products WHERE category = %s ORDER BY id DESC  LIMIT 8;", ['hosting'])
     # latest_productss = cur.fetchall()
     cur.close()
-    return render_template('home.html', latest_products=latest_products, categories=categories)
+    return render_template('home.html', latest_products=latest_products, categories=categories, slider_products_first=slider_products_first, slider_products_second=slider_products_second, slider_products_third=slider_products_third)
 
 
 # user part ***********************************************************************************************
