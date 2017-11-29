d4e3872549669ac05d1deec93c97ca7139886f33
OSAMA.MOHAMED <om1234@fayoum.edu.eg>
2017-11-29 10:31:24 +0200

    make admin able to change his profile picture

--- buy_sell.py
+++ buy_sell.py
@@ -522,7 +522,7 @@ def user_profile_picture():
             dir = app.root_path + r"\static\uploads\users\{}".format(session['user_username'])
             file.save(os.path.join(dir, filename))
             cur = mysql.connection.cursor()
-            cur.execute("UPDATE users SET files = %s WHERE username = %s;", [filename, session['user_username']])
+            cur.execute("UPDATE users SET files = %s WHERE username = %s AND permission = %s;", [filename, session['user_username'], 'user'])
             mysql.connection.commit()
             cur.close()
             flash('You Have successfully uploaded Your Profile Picture!', 'success')
@@ -1015,6 +1015,13 @@ def admin_dashboard():
     cur = mysql.connection.cursor()
 
 
+    # check if admin changed his profile picture
+    cur.execute("SELECT files, permission FROM users WHERE username = %s", [session['admin_username']])
+    files = cur.fetchone()
+    image = files['files']
+    session['admin_image'] = image
+
+
     # count slider products
     cur.execute("SELECT COUNT(id) FROM slider_products")
     sliders = cur.fetchone()
@@ -1124,6 +1131,66 @@ def admin_dashboard():
 
     cur.close()
     return render_template('admin_dashboard.html', count_sliders=count_sliders, count_products=count_products, count_users=count_users, count_categories=count_categories, admin_name=session['admin_username'], admin_image=session['admin_image'], permission=session['permission'], count_number_of_sales=count_number_of_sales, count_number_of_sales_slider=count_number_of_sales_slider, product_saled=product_saled, slider_saled=slider_saled, slider_big=slider_big, slider_small=slider_small, product_big=product_big, product_small=product_small, slider_add=slider_add, product_add=product_add, slider_last_week=slider_last_week, product_last_week=product_last_week, total_avg_rate=total_avg_rate, product_saled_low=product_saled_low, slider_saled_low=slider_saled_low, messages=messages, count_messages=count_messages, count_orders_where_pending=count_orders_where_pending, count_orders_by_user=count_orders_by_user)
+
+
+# admin upload profile picture page
+
+@app.route('/admin/upload_picture', methods=['post', 'get'])
+@is_admin_logged_in
+def upload_picture():
+    cur = mysql.connection.cursor()
+
+    # view messages
+    cur.execute("SELECT * FROM contact_us WHERE status = %s ORDER BY id DESC LIMIT 6;", ["not_seen"])
+    messages = cur.fetchall()
+
+    # show messages number
+    cur.execute("SELECT COUNT(id) FROM contact_us WHERE status = %s ", ['not_seen'])
+    count_message = cur.fetchone()
+    count_messages = count_message['COUNT(id)']
+
+    # show new orders number
+    cur.execute("SELECT COUNT(status) FROM buy_orders WHERE status = %s", ['Pending'])
+    count_order = cur.fetchone()
+    count_orders_where_pending = count_order['COUNT(status)']
+
+    # show new orders
+    cur.execute("SELECT COUNT(status), user_name FROM buy_orders WHERE status = %s GROUP BY user_name ASC LIMIT 12", ['Pending'])
+    count_orders_by_user = cur.fetchall()
+
+    cur.close()
+    return render_template('admin_upload_profile_picture.html', admin_name=session['admin_username'], admin_image=session['admin_image'], permission=session['permission'], messages=messages, count_messages=count_messages, count_orders_where_pending=count_orders_where_pending, count_orders_by_user=count_orders_by_user)
+
+
+# upload admin profile picture
+
+@app.route('/admin/admin_profile_picture', methods=['post'])
+@is_admin_logged_in
+def admin_profile_picture():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part', 'warning')
+            return redirect(url_for('admin_dashboard'))
+        file = request.files['file']
+        if file.filename == '':
+            flash('You Have to Select a File!', 'warning')
+            return redirect(url_for('admin_dashboard'))
+        if file and allowed_file(file.filename):
+            try:
+                rmtree(app.root_path + r"\static\uploads\users\{}".format(session['admin_username']))
+                os.makedirs(app.root_path + r"\static\uploads\users\{}".format(session['admin_username']))
+            except:
+                os.makedirs(app.root_path + r"\static\uploads\users\{}".format(session['admin_username']))
+            filename = secure_filename(file.filename)
+            dir = app.root_path + r"\static\uploads\users\{}".format(session['admin_username'])
+            file.save(os.path.join(dir, filename))
+            cur = mysql.connection.cursor()
+            cur.execute("UPDATE users SET files = %s WHERE username = %s AND permission = %s ;", [filename, session['admin_username'], session['permission']])
+            mysql.connection.commit()
+            cur.close()
+            flash('You Have successfully uploaded Your Profile Picture!', 'success')
+            return redirect(url_for('admin_dashboard'))
+    return redirect(url_for('admin_dashboard'))
 
 
 # product validators form
