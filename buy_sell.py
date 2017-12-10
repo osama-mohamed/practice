e4026a0b7bd38b23d6d317b63de434e6456d29b8
OSAMA.MOHAMED <om1234@fayoum.edu.eg>
2017-12-10 21:36:44 +0200

    add 404 page not found if user entered an invalid url

--- buy_sell.py
+++ buy_sell.py
@@ -198,6 +198,13 @@ def favicon():
                                'admin.png', mimetype='image/vnd.microsoft.icon')
 
 
+# page not found route
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 # home page
 
 @app.route('/', methods=['post', 'get'])
