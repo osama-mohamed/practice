f03488a8379000eaf6772552a822fcb8836e2c08
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-02-05 13:03:12 +0200

    add home page & about me page using templateview cbv

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,6 +10,8 @@ urlpatterns = [
     url(r'^articles/', include('article.urls', namespace='articles')),
     url(r'^accounts/', include('account.urls', namespace='accounts')),
     url(r'^comments/', include('comment.urls', namespace='comments')),
+    url(r'^about_me/$', TemplateView.as_view(template_name='about_me.html'), name='about_me'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
