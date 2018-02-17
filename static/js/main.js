cf121940b42a918f44255c2dd4e3204b407eb3eb
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-02-17 13:16:15 +0200

    calculate body padding

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,13 @@
 $(function () {
+    // calculate body padding top depend on navbar height
+    $('body').css('paddingTop', $('.navbar').innerHeight() + 20 );
+
+    // smooth scroll to element
     $('.navbar li a').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
             scrollTop: $('#' + $(this).data('scroll')).offset().top
         }, 1000);
-        window.console.log('#' + $(this).data('scroll'));
+        // window.console.log('#' + $(this).data('scroll'));
     });
 });
