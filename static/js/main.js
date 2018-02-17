1728a9bca6e3631bda843cccd3030ca48fe63ad3
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-02-17 14:08:19 +0200

    sync active class with elements

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,12 @@
 $(function () {
     // calculate body padding top depend on navbar height
-    $('body').css('paddingTop', $('.navbar').innerHeight() + 20 );
+    $('body').css('paddingTop', $('.navbar').innerHeight());
 
     // smooth scroll to element
     $('.navbar li a').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: $('#' + $(this).data('scroll')).offset().top
+            scrollTop: $('#' + $(this).data('scroll')).offset().top + 1
         }, 1000);
         // window.console.log('#' + $(this).data('scroll'));
     });
@@ -16,5 +16,16 @@ $(function () {
         // $('.navbar a').removeClass('active');
         // $(this).addClass('active');
         $(this).addClass('active').parent().siblings().find('a').removeClass('active');
+    });
+
+    // sync navbar links with sections
+    $(window).scroll(function () {
+        $('.block').each(function () {
+            if($(window).scrollTop() > $(this).offset().top ){
+                var blockID = $(this).attr('id');
+                $('.navbar a').removeClass('active');
+                $('.navbar li a[data-scroll="' + blockID + '"]').addClass('active');
+            }
+        });
     });
 });
