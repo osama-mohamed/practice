5c2cf76eb4c30dd2f2bbf290da572789b3c05089
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-02-23 15:29:51 +0200

    test the input and the output

--- github/tests.py
+++ github/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestCalls(TestCase):
+
+    def test_users(self):
+        response = self.client.post('',
+                                    user_one='OSAMAMOHAMED1234',
+                                    user_two='octocat'
+                                    )
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_users_api(self):
+        response = self.client.post('/api/',
+                                    user_one='OSAMAMOHAMED1234',
+                                    user_two='octocat'
+                                    )
+        self.assertEqual(response.status_code, 200)
