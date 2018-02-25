225d8e3334ee413c957254050ec70704f421d328
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-02-25 08:41:56 +0200

    5 tests on api

--- github/tests.py
+++ github/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .api.serializers import UserSerializer
+from django.core.urlresolvers import reverse
 
 # data = urlencode({"something": "something"})
 # response = self.client.post("/my/form/", data, content_type="application/x-www-form-urlencoded")
@@ -7,11 +7,18 @@ from .api.serializers import UserSerializer
 class TestCalls(TestCase):
 
     def test_users(self):
-        response = self.client.post('',
-                                    user_one='OSAMAMOHAMED1234',
-                                    user_two='octocat'
+        response = self.client.post(reverse('github_detail:home'),
+                                    user_name_one='OSAMAMOHAMED1234',
+                                    user_name_two='octocat'
                                     )
         self.assertEqual(response.status_code, 200)
+        # print(response.content)
+        # print('***************')
+        # print(response.context['login'])
+        # print('------------------------')
+        # print(response.data)
+        print(response.context['user_one'])
+        print(response.context)
 
 
     def test_users_api(self):
@@ -20,17 +27,7 @@ class TestCalls(TestCase):
                                     'user_name_two': 'octocat'}
                                     )
         self.assertEqual(response.status_code, 200)
-        # print(len(response.json()['user_one_stats']))
-        # print(len(response.json()['user_one_stats']['login']))
-        # print(response.json()['user_one_stats'])
-        # print(response.json()['user_one_stats']['login'])
-        # print(len(response.json()))
         self.assertEqual(len(response.json()['user_one_stats']), 30)
         self.assertEqual(response.json()['user_one_stats']['login'], 'OSAMAMOHAMED1234')
+        self.assertEqual(len(response.json()['user_one_stats']['login']), 16)
         self.assertEqual(len(response.json()), 2)
-        # print(response.content)
-        # print(response.context['user_one_stats'])
-        # print(response.json()['user_one_stats'])
-        # print(response.data)
-        # print(response)
-
