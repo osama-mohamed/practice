6dc6aa43c5882c5bb8fb3bdc1bbfae3e3188d216
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-05 20:54:54 +0200

    data type

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 
-var myOldPrice = 1000,
-    myNewPrice = 100,
-    myDiscount = myOldPrice - myNewPrice + 50;
+var hasDiscount = false; // boolean
 
-document.getElementById('price').innerHTML = myDiscount;
+if (hasDiscount === true) {
+    var mainPrice = 350;
+} else {
+    var mainPrice = 500;
+}
+
+document.getElementById('price').innerHTML = mainPrice;
 
 
-var mainPrice = 500,
-    mySmallDiscount = 50,
-    myMediumDiscount = 100,
-    myBigDiscount = 250;
+var social = ['facebook.com', 'youtube.com', 'google.com']; // array
 
-document.getElementById('main-price').innerHTML = mainPrice;
-document.getElementById('product1').innerHTML = mainPrice - mySmallDiscount;
-document.getElementById('product2').innerHTML = mainPrice - myMediumDiscount;
-document.getElementById('product3').innerHTML = mainPrice - myBigDiscount;
+document.getElementById('social-web').innerHTML = social[0];
+
+
+var myFullName = {firstName:'osama', lastName: 'mohamed'}; // object
+
+document.getElementById('full-name').innerHTML = myFullName.firstName;
+
+var myName = "osama \"mohamed\" "; // string   \" to escape from closing the string
+
+document.getElementById('name').innerHTML = myName;
+
+var myAge = 23; // number
+
+document.getElementById('age').innerHTML = myAge;
