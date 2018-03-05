1727dd7f380d8a539a9001f1f56c41d286b4eafa
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-05 23:15:15 +0200

    if, else if, else conditions

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 
-// + add
-// - sub
-// / div
-// * mul
-// % module 
-// ++ increment by 1
-// -- decrement by 1
+var salary = prompt('What is your salary ?');
+
+if (salary <= 1500) {
+   alert('your salary is less than 1500');
+} else if (salary == 1600) {
+    alert('your salary is : 1600');
+} else if (salary == 2000) {
+    alert('your salary is : 2000');
+} else {
+    alert('your salary is : ' + salary);
+}
 
