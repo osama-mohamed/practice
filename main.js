573a16dccc32daf960036ffbccec0720a7fda13b
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-05 21:30:48 +0200

    concatenation

--- main.js
+++ main.js
@@ -1,28 +1,9 @@
 
-var hasDiscount = false; // boolean
+var myName = 'osama',
+    myAge = 23,
+    myCountry = 'Egypt'; // 'osama' + 5 + 3
 
-if (hasDiscount === true) {
-    var mainPrice = 350;
-} else {
-    var mainPrice = 500;
-}
-
-document.getElementById('price').innerHTML = mainPrice;
-
-
-var social = ['facebook.com', 'youtube.com', 'google.com']; // array
-
-document.getElementById('social-web').innerHTML = social[0];
-
-
-var myFullName = {firstName:'osama', lastName: 'mohamed'}; // object
-
-document.getElementById('full-name').innerHTML = myFullName.firstName;
-
-var myName = "osama \"mohamed\" "; // string   \" to escape from closing the string
-
-document.getElementById('name').innerHTML = myName;
-
-var myAge = 23; // number
-
-document.getElementById('age').innerHTML = myAge;
+document.getElementById('test').innerHTML =
+    'My name is : <span style=\"color: red;\">' + myName + '</span><br>' +
+    'My name is : ' + myAge + '<br>' +
+    'My name is : ' + myCountry;
