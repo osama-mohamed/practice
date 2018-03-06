05dbe59e78e843de764fe5d933af5dd29f5fee71
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-06 10:04:04 +0200

    logical operators

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 
-// >    greater than
-// >=   greater than or equal
-// <    less than
-// <=   less than or equal
-// =    assignment (set variable equal to )
-// ==   comparison (compare the value only)
-// ===  identical comparison (compare the value and the data type of the value too)
+/*
+
+ !=   not equal (compare the value only)
+ !==  not identical equal (compare the value and the data type of the value too)
+ &&   and
+ ||   or
+
+ */
