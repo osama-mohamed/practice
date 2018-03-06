672cdeeeddec3ab6778344a3d0a415ecfa8546c6
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-06 21:37:29 +0200

    switch, case, break

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
+var season = prompt('what is your favourite season ?');
 
-(function sayWelcome() {
-    alert('welcome');
-})();   // self invoke method 1
-
-
-(function convertPrice() {
-    var myPrice = document.getElementById('price').innerHTML;
-    alert(myPrice * 3.75)
-}());   // self invoke method 2
-
-
-function convertDollar() {
-    var amount = document.getElementById('dollar').value,
-        result = amount * 20,
-        message = document.getElementById('convert-result');
-    if (amount === '') {
-        message.innerHTML = 'This field can not be empty';
-    } else if (isNaN(amount)) {
-        message.innerHTML = 'This field accept numbers only';
-    } else if (amount === '0') {
-        message.innerHTML = 'Number can not be 0';
-    } else if (amount < 0) {
-        message.innerHTML = 'Number can not be less than 0';
-    } else {
-        message.innerHTML = amount + ' dollar is equal to ' + result + ' LE';
-    }
+switch (season) {
+    case 'winter':
+        alert('winter is very cold!');
+        break;
+    case 'summer':
+        alert('summer is very hot!');
+        break;
+    case 'spring':
+    case 'autumn':
+        alert('This season is amazing!');
+        break;
+    default:
+        alert('You did not write a valid season name!');
+        break;
 }
