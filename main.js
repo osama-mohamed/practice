5a273fb48b7fdec6db408cee2789243bcf301366
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-06 21:14:38 +0200

    function self invoke

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 
-function myAgeInHours(myAge) {
-    return myAge * 365 * 24;
+(function sayWelcome() {
+    alert('welcome');
+})();   // self invoke method 1
+
+
+(function convertPrice() {
+    var myPrice = document.getElementById('price').innerHTML;
+    alert(myPrice * 3.75)
+}());   // self invoke method 2
+
+
+function convertDollar() {
+    var amount = document.getElementById('dollar').value,
+        result = amount * 20,
+        message = document.getElementById('convert-result');
+    if (amount === '') {
+        message.innerHTML = 'This field can not be empty';
+    } else if (isNaN(amount)) {
+        message.innerHTML = 'This field accept numbers only';
+    } else if (amount === '0') {
+        message.innerHTML = 'Number can not be 0';
+    } else if (amount < 0) {
+        message.innerHTML = 'Number can not be less than 0';
+    } else {
+        message.innerHTML = amount + ' dollar is equal to ' + result + ' LE';
+    }
 }
-
-var askForAge = prompt('what is your age ? ');
-
-document.getElementById('test').innerHTML = 'My Age in Hours = ' + myAgeInHours(askForAge) + ' Hours';
