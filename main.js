c6ad79f9816c4d8038789a96dee477f554b70750
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-07 07:48:44 +0200

    events part 2 : onkeydown, onkeypress and onkeyup

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 
+var myInput = document.getElementById('test-input'),
+    myDiv = document.getElementById('test');
+
+myInput.onkeyup = function () {
+    myDiv.innerHTML = myInput.value * 20;
+};
