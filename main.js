d7fb2a45edaf8685225d7a232637573eed83af9d
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-07 18:13:42 +0200

    array part 8 : concat and slice array

--- main.js
+++ main.js
@@ -15,8 +15,24 @@ var friends = [
 
 console.log(friends);
 
-friends.sort();
-console.log(friends);
+var sliced= friends.slice(2, 4);    // (start, stop) from first
+console.log(sliced);
 
-friends.reverse();
-console.log(friends);
+var slicedFromEnd= friends.slice(-10, -6);    // (start, stop) from last
+console.log(slicedFromEnd);
+
+var newFriends = [
+    'Ebram',
+    'Mikel',
+    'Mina'
+];
+
+var workFriends = [
+    'Mariem',
+    'Basma',
+    'norhan'
+];
+
+// concatenation newFriends and workFriends to friends
+var allFriends = friends.concat(newFriends, workFriends);
+console.log(allFriends);
