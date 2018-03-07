bf787201f810ca8caaacc43e8436d305a8263005
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-07 08:14:46 +0200

    events part 3 : onmouseover, onmouseout and onchange

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 
 var myInput = document.getElementById('test-input'),
-    myDiv = document.getElementById('test');
+    myDiv = document.getElementById('test'),
+    myCurrency = document.getElementById('currency');
 
-myInput.onkeyup = function () {
-    myDiv.innerHTML = myInput.value * 20;
+
+// myDiv.onmouseover = function () {
+//     myDiv.innerHTML = 'You hovered me';
+// };
+//
+// myDiv.onmouseout = function () {
+//     myDiv.innerHTML = 'You moved out of me';
+// };
+//
+// myInput.onkeyup = function () {
+//     myDiv.innerHTML = myInput.value * 20;
+// };
+
+myCurrency.onchange = function () {
+    myDiv.innerHTML = myInput.value * myCurrency.value;
 };
