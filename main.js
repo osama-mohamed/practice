21407b67c49d05bcb30ece7535fcb6abecd08179
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-07 07:19:22 +0200

    events part 1 : onload, onclick and on dblclick

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
 
-var x = 1;
-
-function xParent() {
-    var x = 2;
-    var cal = x + 2;
-    function xTest() {
-        var x = 5,
-            cal = x + 2;
-        console.log(cal);
-    }
-    return xTest();
-}
-
-console.log(x);
-xParent();
-console.log(x);
-
-
