898c0bc190ac6dd3fe3582cdabe480fcccf84cb6
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-07 17:28:26 +0200

    array part 5 : insert items to array

--- main.js
+++ main.js
@@ -6,24 +6,29 @@ var friends = [
 ];
 
 console.log(friends);
+console.log(friends.length);
 
-friends = friends.toString();            // convert array to string
+
+friends[3] = 'Osama';
 console.log(friends);
+console.log(friends.length);
 
-friends = friends.toLocaleString();     // convert array to string
+friends[friends.length] = 'Sara';   // insert Sara at the last
 console.log(friends);
+console.log(friends.length);
 
-friends = friends.join();               // convert array to string
+friends.push('Nariman');            // insert Nariman at the last
 console.log(friends);
+console.log(friends.length);
 
-friends = friends.join('-');            // convert array to string
+friends.unshift('Mai');             // insert Mai at the first
 console.log(friends);
+console.log(friends.length);
 
+friends.splice(5, 1);              // (start on index, number of how many to delete)
+console.log(friends);
+console.log(friends.length);
 
-var myDate = new Date(),
-    myString = myDate.toString(),
-    myLocaleString = myDate.toLocaleString();
-
-console.log(myDate);
-console.log(myString);
-console.log(myLocaleString);
+friends.splice(5, 0, 'Ahmed', 'Ali', 'Mona'); // (start on index, don't delete, add something)
+console.log(friends);
+console.log(friends.length);
