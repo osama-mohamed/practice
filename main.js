06f4d41378606b385f40ba2650be6257184af330
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-09 18:31:10 +0200

    String Methods part 6 : touppercase, tolowercase

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 /*
-    String.fromCharCode(num1, num2, num3, num4, ...........)
-    concat(string, string, ....)
+    toLowerCase()
+    toUpperCase()
 */
 
-var myName = String.fromCharCode(79, 115, 97, 109, 97),
-    myNameCap = String.fromCharCode(79, 83, 65, 77, 65),
-    myString = 'This is a string',
-    myStringTwo = 'This is a second string',
-    concatString = myString.concat(' ', myStringTwo),
-    concatStringTwo = 'This is a string'.concat(' ', 'This is a second string');
+var myString = 'This is a string',
+    myLowerString = myString.toLowerCase(),
+    myUpperString = myString.toUpperCase();
 
-console.log(myName);
-console.log(myNameCap);
+console.log(myString);
+console.log(myLowerString);
+console.log(myUpperString);
 
-console.log(concatString);
-console.log(concatStringTwo);
+
+console.log("THIS IS A STRING".toLowerCase());
+console.log("this is a string".toUpperCase());
