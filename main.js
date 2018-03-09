864de51e1ad1a9b8d83f2837513c07ea808cf588
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-09 20:39:03 +0200

    Multiplication table : practice on for loop

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 var x,y;
-for (x=1; x<=12; x+=1) {
-    for (y=1; y<=12; y+=1) {
+for (x = 1; x <= 12; x += 1) {
+    for (y = 1; y <= 12; y += 1) {
         console.log(x, y);
     }
     console.log('*********')
@@ -9,8 +9,8 @@ for (x=1; x<=12; x+=1) {
 
 
 var c,v;
-for (c=12; c>=1; c-=1) {
-    for (v=12; v>=1; v-=1) {
+for (c = 12; c >= 1; c -= 1) {
+    for (v = 12; v >= 1; v -= 1) {
         console.log(c, v);
     }
     console.log('=======')
