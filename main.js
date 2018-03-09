a7607ebcbfee2343799cad53c4b8d30383129890
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-09 20:56:03 +0200

    for in loop

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 
-var x,y;
-for (x = 1; x <= 12; x += 1) {
-    for (y = 1; y <= 12; y += 1) {
-        console.log(x, y);
-    }
-    console.log('*********')
-}
+var newCar = {
+    color: 'black',
+    type: 'sedan',
+    model: 2018
+},
+    prop;
 
-
-var c,v;
-for (c = 12; c >= 1; c -= 1) {
-    for (v = 12; v >= 1; v -= 1) {
-        console.log(c, v);
+for (prop in newCar) {
+    if (newCar.hasOwnProperty(prop)) {
+        console.log(prop + " : " + newCar[prop])
     }
-    console.log('=======')
 }
