6fb41a5bdc10f588df310e561b36c3acc966a14c
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-10 09:21:14 +0200

    ways to access objects

--- main.js
+++ main.js
@@ -1,36 +1,18 @@
 
+var newCar = {
+    color: 'black',
+    type: 'sedan',
+    model: 2018
+},
+    prop;
 
-var i = 0;
-for (; i <= 10; i += 1) {
-    if (i === 4) {
-        break;
+console.log(newCar);
+console.log(newCar.model);
+console.log(newCar['model']);
+
+
+for (prop in newCar) {
+    if (newCar.hasOwnProperty(prop)) {
+        console.log(prop + " : " + newCar[prop])
     }
-    console.log(i);
-}
-
-
-var a = 0;
-for (; a <= 10; a += 1) {
-    if (a === 4) {
-        continue;
-    }
-    console.log(a);
-}
-
-
-var x,y;
-
-MainLoop:
-for (x = 1; x <= 12; x += 1) {
-    ChildLoop:
-    for (y = 1; y <= 12; y += 1) {
-        if (y === 6) {
-            // break ChildLoop;
-            // break MainLoop;
-            // continue ChildLoop;
-            // continue MainLoop;
-        }
-        console.log(x, y);
-    }
-    console.log('*********')
 }
