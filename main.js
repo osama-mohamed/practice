1dc50c2553287c06e35a286eda28d6df77ed7f93
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-10 10:52:41 +0200

    regular expression part 3 : quantifiers

--- main.js
+++ main.js
@@ -16,10 +16,14 @@
     [^0-9]      => not range from 0 to 9
     [A-g]       => range [A-Z] and range [a-g]
     [0-9a-z]    => range [0-9] and range [a-z]
+    letter+                      => word contain more than number of letter
+    letter{number}               => word contain number of letter
+    letter{number1, number2}     => word contain number1 of letter or number2 of letter
+    letter{number,}              => word contain more than number of letter
 */
 
 
-var myString = 'This 1 is 2 a 6 string 9',
-    result = myString.replace(/[^a-z0-9 ]/g, '@');
+var myString = 'This 1 ihhs 2 ahhh 6 sthhhhring 9',
+    result = myString.replace(/h{2,}/g, '@');
 
 console.log(result);
