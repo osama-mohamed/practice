abb820876b70e3361b7721c8cd8edd79158fbd97
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-10 08:23:16 +0200

    while loop

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 
 
-function generateYears(Start, End) {
-    document.write('<select>');
-        var year;
-        for (year = Start; year <= End; year += 1) {
-            document.write("<option value=\"" + year + "\">" + year + "</option>");
-            // document.write("<option value=" + year + ">" + year + "</option>");
-        }
-    document.write('</select>');
-}
-
-generateYears(1900, 2020);
-
-
-function oneClick() {
-    var startYear = document.getElementById('year-start').value;
-    var endYear = document.getElementById('year-end').value;
-    generateYears(parseInt(startYear), parseInt(endYear));
+var i = 0;
+while (i <= 10) {
+    console.log(i);
+    i += 1;
 }
