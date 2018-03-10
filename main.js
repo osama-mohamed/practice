b33c3b1476dc59eccdc9c5f1dcc531f1c7a15315
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-10 21:05:58 +0200

    hide placeholder on input focus

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
 
 
-var seconds = 122,
-    countDiv = document.getElementById('countdown'),
-    countDown = setInterval(function () {
-        secondPass();
-    }, 1000);
+var myInput = document.getElementById('my-input');
 
-function secondPass() {
-    var minutes = Math.floor(seconds / 60),
-        remSeconds = seconds % 60;
-    if (remSeconds < 10) {            // if second less than 10 > put 0 before it
-        remSeconds = '0' + remSeconds;
-    }
-    if (minutes < 10) {               // if minute less than 10 > put 0 before it
-        minutes = '0' + minutes;
-    }
-    countDiv.innerHTML = minutes + ' : ' + remSeconds;   //put the counter inside the div
-    if (seconds > 0) {                // if there is time > decrease it
-        seconds -= 1;                 // decrease one second every 1000 milliseconds
-    } else {
-        clearInterval(countDown);     // stop count down timer
-        countDiv.innerHTML = 'Done';  // message to appear when timer ended
-    }
-}
+myInput.onfocus = function () {
+    this.setAttribute('data-place', this.getAttribute('placeholder'));
+    this.setAttribute('placeholder', '');
+};
+
+myInput.onblur = function () {
+    this.setAttribute('placeholder', this.getAttribute('data-place'));
+    this.setAttribute('data-place', '');
+};
