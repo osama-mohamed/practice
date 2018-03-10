e6e1660d3d10b67125c0c62b85fe92296038f64d
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-10 13:11:33 +0200

    Date part 4 : date functions 2 set

--- main.js
+++ main.js
@@ -1,35 +1,22 @@
 
 /*
-    getDate = day of the month from 1 to 31
-    getDay = day of the week from 0 to 6 >> sun == 0 & sat == 6
-    getFullYear
-    getHours
-    getMinutes
-    getSeconds
-    getMilliseconds
-    getTime = milliseconds from 1 jan 1970 until now
-    getTimezoneOffset = difference between utc and local time in minutes
+    setDate(day[Req]) = day of the month from 1 to 31
+    setFullYear(year[Req], month[Opt](0-11 index), day[Opt])
+    setHours(hour[Req], minutes[Opt], seconds[Opt], milliseconds[Opt])
+    setMinutes(minutes[Req], seconds[Opt], milliseconds[Opt])
+    setSeconds(seconds[Req], milliseconds[Opt])
+    setMilliseconds(milliseconds[Req])
+    setMonth(month[Req](0-11 index), day[opt])
 */
 
 
-var currentDate  = new Date(),
-    day = currentDate.getDate(),
-    dayOfWeek = currentDate.getDay(),
-    fullYear = currentDate.getFullYear(),
-    hours = currentDate.getHours(),
-    minutes = currentDate.getMinutes(),
-    seconds = currentDate.getSeconds(),
-    milliSeconds = currentDate.getMilliseconds(),
-    milliSecondsFrom1970 = currentDate.getTime(),
-    timeZoneOffset = currentDate.getTimezoneOffset();
+var currentDate  = new Date();
+// currentDate.setDate(1);
+// currentDate.setFullYear(2016, 9, 1);
+// currentDate.setHours(16, 45,45);
+// currentDate.setMinutes(44, 46);
+// currentDate.setSeconds(42);
+// currentDate.setMilliseconds(600000); // 10 minutes
+// currentDate.setMonth(3);
 
 console.log(currentDate);
-console.log(day);
-console.log(dayOfWeek);
-console.log(fullYear);
-console.log(hours);
-console.log(minutes);
-console.log(seconds);
-console.log(milliSeconds);
-console.log(milliSecondsFrom1970);
-console.log(timeZoneOffset);
