f75a032a91da5ecee02fabf2bc96d82e6b7be291
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-10 08:26:07 +0200

    enter start and end year to create years select box with while loop

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ while (i <= 10) {
     console.log(i);
     i += 1;
 }
+
+function generateYears(Start, End) {
+    document.write('<select>');
+        var year = Start;
+        while (year <= End) {
+            document.write("<option value=\"" + year + "\">" + year + "</option>");
+            // document.write("<option value=" + year + ">" + year + "</option>");
+            year += 1
+        }
+    document.write('</select>');
+}
+
+generateYears(1900, 2020);
+
+
+function oneClick() {
+    var startYear = document.getElementById('year-start').value;
+    var endYear = document.getElementById('year-end').value;
+    generateYears(parseInt(startYear), parseInt(endYear));
+}
