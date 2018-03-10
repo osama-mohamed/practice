2392e59b4b8b95374526d5607ba69494315de631
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-10 11:39:27 +0200

    Date part 1 : intro

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
 
 /*
-    regular expression syntax
-    /pattern/attributes
-
-    Attributes List :
-    i   => case insensitive
-    g   => global
-    m   => multi line search
-
-    [char]      => character
-    [^char]     => not character
-    [a-zA-Z]    => range small and capital letters
-    [^a-zA-Z]   => not range small and capital letters
-    [0-9]       => range from 0 to 9
-    [^0-9]      => not range from 0 to 9
-    [A-g]       => range [A-Z] and range [a-g]
-    [0-9a-z]    => range [0-9] and range [a-z]
-    letter+                      => word contain more than number of letter
-    letter{number}               => word contain number of letter
-    letter{number1, number2}     => word contain number1 of letter or number2 of letter
-    letter{number,}              => word contain more than number of letter
+    new Date()                  => current date and time
+    new Date(10000)             => 10000 milliseconds == 10 seconds
+    new Date("dateString")      => date as a string
+    new Date(year, month, day, hour, minute, second, millisecond)      => date as a string
 */
 
 
-var myString = 'This 1 ihhs 2 ahhh 6 sthhhhring 9',
-    result = myString.replace(/h{2,}/g, '@');
+var currentDate = new Date(),
+    milliseconds = new Date(1000),
+    dateString  = new Date('october 01 1994 00:00:00'),
+    numberedDate = new Date(1994, 9, 1, 0, 0, 0, 0);
 
-console.log(result);
+console.log(currentDate);
+console.log(milliseconds);
+console.log(dateString);
+console.log(numberedDate);
