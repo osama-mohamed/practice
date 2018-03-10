e3901a52e8c576f6f2e3949c30e1ca4edc5eeb59
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-10 20:08:26 +0200

    count down timer

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 
-/*
-    toString()
-    toExponential() // 1000 === 10 ^ 3
-    toFixed()       // round number depend on number in ()
-    parseInt()      // number should be at first
-*/
 
+var seconds = 122,
+    countDiv = document.getElementById('countdown'),
+    countDown = setInterval(function () {
+        secondPass();
+    }, 1000);
 
-console.log((200).toString());
-console.log((200).toExponential());
-console.log((9.56).toFixed(0));
-console.log((9.56).toFixed(1));
-console.log(parseInt('600px'));
+function secondPass() {
+    var minutes = Math.floor(seconds / 60),
+        remSeconds = seconds % 60;
+    if (remSeconds < 10) {            // if second less than 10 > put 0 before it
+        remSeconds = '0' + remSeconds;
+    }
+    if (minutes < 10) {               // if minute less than 10 > put 0 before it
+        minutes = '0' + minutes;
+    }
+    countDiv.innerHTML = minutes + ' : ' + remSeconds;   //put the counter inside the div
+    if (seconds > 0) {                // if there is time > decrease it
+        seconds -= 1;                 // decrease one second every 1000 milliseconds
+    } else {
+        clearInterval(countDown);     // stop count down timer
+        countDiv.innerHTML = 'Done';  // message to appear when timer ended
+    }
+}
