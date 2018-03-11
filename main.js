7e5bb3289b7a2190771d9a20839931daffb34d31
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-11 17:47:02 +0200

    calculate remaining characters in textarea, if condition in short code

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 
-var myElement = document.getElementById('myImg'),
-    myImgs = [
-        'img/js.png',
-        'img/python2.png',
-        'img/mysql.png',
-        'img/mariadb.png',
-        'img/5.png',
-        'img/CSS33.png'
-    ];
+var myTextarea = document.getElementById('myarea'),
+    mySpan = document.getElementById('myspan');
 
 
-function changeImg(myElement, myImgs) {
-    setInterval(function () {
-        var myRandomNumber = Math.floor(Math.random() * myImgs.length)
-        myElement.src = myImgs[myRandomNumber];
-        console.log(myRandomNumber);
-        console.log(myImgs[myRandomNumber]);
-    }, 1000);
-}
-changeImg(myElement, myImgs);
+myTextarea.onkeyup = function () {
+    mySpan.textContent = 70 - this.value.length;
 
+    // if condition in short code
+    mySpan.textContent < 0 ? mySpan.style.color = '#f00' : mySpan.style.color = '#0f0';
+
+    // // if condition in long code
+    // if (mySpan.textContent < 0) {
+    //         mySpan.style.color = '#f00';
+    // } else if (mySpan.textContent >= 0) {
+    //         mySpan.style.color = '#0f0';
+    // }
+};
