b7dbe0f371836a4edec14539ef9d28a798fc2164
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-11 16:33:37 +0200

    check if url hash contains string

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 
-
-function redirect(url) {
-    if (url !== '') {
-        window.location = url;
+// first method
+if (window.location.hash) {
+    if (window.location.hash.indexOf('osama') === 1) {
+        window.location = 'https://www.fb.com/osama.mohamed.ms';
     }
+} else {
+    console.log('no hash is found here');
+}
+
+
+// second method
+if (window.location.hash) {
+    var hash = window.location.hash.substring(1);
+    console.log(hash);
+    if (hash === 'osama') {
+        window.location = 'https://www.fb.com/osama.mohamed.ms';
+    }
+} else {
+    console.log('no hash is found here too');
 }
