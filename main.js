aecf89c4209e5ae8b9b93a6d7b05f830e257ed3b
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-11 19:10:43 +0200

    edit text live on input

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 
-document.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
-});
+var myTitle = document.getElementById('title'),
+    myContent = document.getElementById('content'),
+    myLiveTitle = document.getElementById('title-live'),
+    myLiveContent = document.getElementById('content-live');
+
+myTitle.onkeyup = function () {
+    myLiveTitle.textContent = this.value;
+};
+
+myContent.onkeyup = function () {
+    myLiveContent.textContent = this.value;
+};
