b2c7e3a99e587875a7bdc99988e356aa315a6af3
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-11 18:37:17 +0200

    disable right click on page

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
 
-function showTime() {
-    var time = new Date(),
-    hour = time.getHours(),
-    minute = time.getMinutes(),
-    second = time.getSeconds();
-    if (hour < 10) {
-        hour = '0' + hour;
-    }
-    if (minute < 10) {
-        minute = '0' + minute;
-    }
-    if (second < 10) {
-        second = '0' + second;
-    }
-    document.getElementById('clock').innerHTML = hour + ' : ' +
-                                                 minute + ' : ' +
-                                                 second;
-}
-
-window.onload = function () {
-    setInterval(showTime, 500);
-};
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
