df37a8ce556e0db6af0a7701f8e3355fe2f06ca8
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-11 16:12:31 +0200

    redirect function

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 
 
-function generateSerial() {
-    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-        serialLength = 20,
-        randomSerial = '',
-        randomNumber,
-        i = 0;
-    for (; i < serialLength; i += 1) {
-        randomNumber = Math.floor(Math.random() * chars.length);
-        randomSerial += chars.substring(randomNumber, randomNumber + 1);
+function redirect(url) {
+    if (url !== '') {
+        window.location = url;
     }
-    document.getElementById('serial').innerHTML = randomSerial;
 }
