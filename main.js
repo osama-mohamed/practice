449a95c9ebf9573d4b22aa1431621ca286490c50
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-12 11:13:14 +0200

    window methods part 1 : alert, confirm, prompt

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-/*
 
-var myInput = document.getElementById('text-input');
+// window.alert('Hello \nOSAMA MOHAMED \nPython Developer');
+alert('Hello \nOSAMA MOHAMED \nPython Developer \nFull stack developer');
 
-myInput.onfocus = function () {
-    if (this.placeholder === 'Type Your Text Here') {
-        this.placeholder = '';
-    }
-};
 
-myInput.onblur = function () {
-    if (this.placeholder === '') {
-        this.placeholder = 'Type Your Text Here';
-    }
-};
+// var ask = window.confirm('Are you sure you want to open Google ?');
+var ask = confirm('Are you sure you want to open \nGoogle ?');
+if (ask === true) {
+    window.location.href = 'https://www.google.com.eg';
+} else {
+    console.log('sorry you choosed not to open Google');
+    // window.console.log('sorry you choosed not to open Google');
+}
 
-*/
+
+// var name = window.prompt('enter Your name', 'Example: Osama');  // return strings
+var name = prompt('enter Your name', 'Example: Osama');      // return strings
+console.log(name);
