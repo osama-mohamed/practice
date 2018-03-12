b8c22c373f146d1dcc8c3a0387943c2b1f99b191
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-12 20:29:19 +0200

    window document cookie part 2 : read, create, update, delete

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 /*
 	window.document.cookie > read cookies
-	window.document.cookie('name=value; expires=Dtae; path=') set cookie
+	window.document.cookie('name=value; expires=Wed Mar 14 2018 20:10:50 GMT+0200; path=/') set cookie
 */
 
 
 window.console.log(window.document.cookie);
-window.console.log(window.document.cookie = 'name=OSAMA');
+window.console.log(window.document.cookie = 'name=OSAMA; expires=Mon Mar 12 2018 22:10:50 GMT+0200; path=/'); // create
+window.console.log(window.document.cookie = 'name=OSAMA Mohamed; expires=Mon Mar 15 2018 22:10:50 GMT+0200; path=/'); // update
+window.console.log(window.document.cookie = 'name=; expires=Mon Mar 12 2018 20:10:50 GMT+0200; path=/'); // delete
 window.console.log(window.document.cookie);
 
