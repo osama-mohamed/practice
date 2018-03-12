e488706c6d6e453886590cba1f84b049d677c13f
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-12 17:24:39 +0200

    window location properties part 3 : protocol, pathname, search

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 /*
-	window.location.host;
-	window.location.hash;
+	window.location.protocol;	> (http, https, file, mailto, ftp)
+	window.location.search;		> (?q=something&page=2)
+	window.location.pathname;	> (/admin/edit/profile/1)
 */
 
 
-window.console.log(window.location.host);
-window.console.log(window.location.hash);
+window.console.log(window.location.protocol);
+window.console.log(window.location.search);
+window.console.log(window.location.pathname);
