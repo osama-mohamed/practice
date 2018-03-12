27cc019831474d643c7e3f5794b33fbca5bb8422
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-12 18:58:01 +0200

    window location methods : assign, replace, reload

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 /*
-	window.location.protocol;	> (http, https, file, mailto, ftp)
-	window.location.search;		> (?q=something&page=2)
-	window.location.pathname;	> (/admin/edit/profile/1)
+	window.location.assign('https://www.google.com.eg'); > redirect to the link and do not delete history
+	window.location.replace('https://www.google.com.eg'); > redirect to the link and delete history
+	window.location.reload();	    > reload current page from browser cache
+	window.location.reload(false);	> reload current page from browser cache
+	window.location.reload(true);   > reload current page from live server (hard reload)
 */
 
 
-window.console.log(window.location.protocol);
-window.console.log(window.location.search);
-window.console.log(window.location.pathname);
+window.location.assign('https://www.google.com.eg');
+window.location.replace('https://www.google.com.eg');
+window.location.reload();
+window.location.reload(true);
