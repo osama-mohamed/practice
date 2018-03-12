ce9154e1dd178568485573c2ba50637a65644ae4
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-12 12:48:47 +0200

    window methods part 4 : open

--- main.js
+++ main.js
@@ -1,42 +1,18 @@
 /*
-    setInterval(function, milliseconds, param1, para2, param3, ......)
-    clearInterval(id or number of setInterval function)
+	window.open(
+	url,
+	tab-name or target-attribute(_blank[default], _self, ..),
+	specification,
+	history replace (true or false)
+	)
 */
 
 
-setInterval(function () {
-    console.log('Hello osama mohamed every 3 seconds!')
-}, 3000);
-
-
-function writeHello() {
-    console.log('Hello osama mohamed every 5 seconds!')
-}
-
-var myButton = document.getElementById('click'),
-    myMessage = setInterval(writeHello, 5000),
-    myDiv = document.getElementById('count'),
-    myCountDown = setInterval(countDown, 1000);
-
-console.log(myMessage);
-
-myButton.onclick = function () {
-    clearInterval(myMessage);
-    // clearInterval(2);
-    console.log(myMessage);
+document.getElementById('click').onclick = function () {
+	window.open(
+		'https://www.google.com.eg',
+		'OSAMA MOHAMED',
+		'width=600, height=400, left=500, top=100, menubar=yes, status=no',
+		'true'
+	);
 };
-
-
-
-
-function countDown() {
-    if (myDiv.textContent <= 1) {
-        myDiv.textContent = 'Done';
-        clearInterval(myCountDown);
-    } else {
-        myDiv.textContent -= 1;
-        if (myDiv.textContent < 10) {
-            myDiv.textContent = '0' + myDiv.textContent;
-        }
-    }
-}
