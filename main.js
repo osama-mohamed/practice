985ece2ec1126be3b1d1d52120a1c27e6df6eb88
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-13 21:02:57 +0200

    elements children part 2 : firstElementChild, lastElementChild, firstChild, lastChild

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 /*
-	childElementCount > get number of children of element (elements only)
-	children.length   > get number of children of element (elements only)
-	childNodes.length > get number of children of element (elements, text, comments, newlines)
+	firstElementChild   > get first element child of element (elements only)
+	lastElementChild    > get last element child of element (elements only)
+
+	firstChild          > get first child of element  (elements, text, comments, newlines)
+	lastChild           > get last child of element (elements, text, comments, newlines)
 */
 
 
 var myDiv = document.querySelector('div');
 
-console.log(myDiv.childElementCount);
-console.log(myDiv.children.length);
-
-
-console.log(myDiv.children);
+console.log(myDiv.firstElementChild);
 console.log(myDiv.children[0]);
-console.log(myDiv.children[0].textContent);
+
+console.log(myDiv.lastElementChild);
+console.log(myDiv.children[1]);
 
 
-console.log(myDiv.childNodes);
-console.log(myDiv.childNodes.length);
+console.log(myDiv.firstChild);
+console.log(myDiv.childNodes[0]);
+
+console.log(myDiv.lastChild);
+console.log(myDiv.childNodes[5]);
+
