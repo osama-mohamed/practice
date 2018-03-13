ea8f867e9377c799356d5b4a46322ab6e0966a03
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-13 19:24:27 +0200

    elements classlist part 1 : item, contains

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 /*
-	hasAttribute('id');             > check (true, false)
-	removeAttribute('attribute');   > remove
+	classList;                  > get array of element classes
+	classList.value;            > get classes as a string
+	classList.contains('osama') > check if class is exists ('class name)
+	classList.item(3)           > get class name of index as a string
 */
 
 
 var myDiv = document.querySelector('div');
 
-console.log(myDiv.hasAttribute('id'));
-console.log(myDiv.hasAttribute('data-name'));
+console.log(myDiv.classList);
+console.log(myDiv.classList.value);
+console.log(myDiv.classList.length);
+console.log(myDiv.classList.item(3));
 
-myDiv.removeAttribute('class');
-
-if (myDiv.hasAttribute('id')) {
-	console.log('yes');
-} else {
-	console.log('no');
+if (myDiv.classList.contains('osama')) {
+	console.log('Hello osama');
 }
-
