b05537380a30481a306f38ed814c18a35ccf9306
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-13 16:38:17 +0200

    find elements part 1 : by id, class, tag

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 /*
-	window.document.cookie > read cookies
-	window.document.cookie('name=value; expires=Wed Mar 14 2018 20:10:50 GMT+0200; path=/') set cookie
+	document.getElementById('div-1')        > ('element id')
+	document.getElementsByTagName('div')    > ('element tag name')
+	document.getElementsByClassName('test') > ('class name')
+	document.querySelectorAll('div.test')   > ('element tag name + .class name OR #id')
 */
 
-
-window.console.log(window.document.cookie);
-window.console.log(window.document.cookie = 'name=OSAMA; expires=Mon Mar 12 2018 22:10:50 GMT+0200; path=/'); // create
-window.console.log(window.document.cookie = 'name=OSAMA Mohamed; expires=Mon Mar 15 2018 22:10:50 GMT+0200; path=/'); // update
-window.console.log(window.document.cookie = 'name=; expires=Mon Mar 12 2018 20:10:50 GMT+0200; path=/'); // delete
-window.console.log(window.document.cookie);
-
+document.getElementById('div-1').innerHTML = 'OSAMA MOHAMED';
+console.log(document.getElementsByTagName('div'));
+console.log(document.getElementsByClassName('test'));
+console.log(document.querySelectorAll('div.test'));
