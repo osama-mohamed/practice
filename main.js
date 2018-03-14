ca0076702093f43767bce356c6844ca3c3e0bc29
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-14 09:53:28 +0200

    elements : focus, blur

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 /*
-	nextElementSibling       > get the next sibling of element only      (recommended to use)
-	previousElementSibling   > get the previous sibling of element only  (recommended to use)
-
-	nextSibling       > get the next sibling of node, element
-	previousSibling   > get the previous sibling of node, element
+	focus()  > focus on the element
+	blur()   > blur from the element
 */
 
 
-var myDiv = document.getElementById('test');
+var myForm = document.getElementById('test'),
+	myInput = document.getElementById('my-input'),
+	myButton = document.querySelector('button');
 
 
-console.log(myDiv.childNodes[1].nextElementSibling);
-console.log(myDiv.childNodes[3].previousElementSibling);
+window.onload = function () {
+	myInput.focus();
+};
 
-console.log(myDiv.childNodes[1].nextSibling);
-console.log(myDiv.childNodes[1].previousSibling);
-
-
-myDiv.childNodes[3].previousElementSibling.textContent += ' changed by js';
+myButton.onclick = function () {
+	myInput.blur();
+};
