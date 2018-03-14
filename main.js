e36deeacba3b7caa43b6fc67e58a99996efc08f1
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-14 15:21:58 +0200

    elements client : clienttop, clientleft

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 /*
-	scrollTop    > scroll from top number of pixels
-	scrollLeft   > scroll from left number of pixels
+	clientTop    > get border in pixels
+	clientLeft   > get border and scroll in pixels
 */
 
 var myDiv = document.getElementById('main');
 
 
-myDiv.onclick = function () {
-	document.documentElement.scrollTop += 100;
-	console.log(document.documentElement.scrollTop);
-
-	if (document.documentElement.scrollTop >= 1000) {
-		this.classList.add('active');
-	}
-};
+console.log(myDiv.clientTop);
+console.log(myDiv.clientLeft);
