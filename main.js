8aa50347e8b7b5c682af80735570b9bdd5c9d8cc
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-14 08:35:25 +0200

    elements nodes : nodename, nodevalue, nodetype, tagname

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 /*
-	removeChild(index)  > remove child element from parent element (elements, text)
+	tagName     > get name of element tags only
+	nodeName    > get name of tags, text, comment
+	nodeValue   > get value of tags, text, comment
+	nodeType    > get type of tags, text, comment
+	nodeType :
+		1 = tag
+		2 = attribute
+		3 = text
+		8 = comment
 */
 
 
 var myDiv = document.getElementById('test');
 
+console.log(myDiv.childNodes[0].nodeName);
+console.log(myDiv.childNodes[1].tagName);
 
-myDiv.removeChild(myDiv.firstElementChild);
-myDiv.removeChild(myDiv.childNodes[1]);
-myDiv.removeChild(myDiv.children[0]);
+console.log(myDiv.childNodes[0].nodeValue);
+console.log(myDiv.childNodes[1].childNodes[0].nodeValue);
+
+console.log(myDiv.childNodes[0].nodeType);
