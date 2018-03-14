893b031a7a6899b76d84e7572a0ce192b61ddc41
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-14 15:11:27 +0200

    elements scroll : scrolltop, scrollleft

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 /*
-	offsetHeight
-	offsetWidth
-		include :
-			- padding
-			- visible area
-			- border
-			- scroll
-		exclude :
-			- margin
+	scrollTop    > scroll from top number of pixels
+	scrollLeft   > scroll from left number of pixels
 */
-
 
 var myDiv = document.getElementById('main');
 
 
-console.log(myDiv.offsetHeight);
-console.log(myDiv.offsetWidth);
+myDiv.onclick = function () {
+	document.documentElement.scrollTop += 100;
+	console.log(document.documentElement.scrollTop);
+
+	if (document.documentElement.scrollTop >= 1000) {
+		this.classList.add('active');
+	}
+};
