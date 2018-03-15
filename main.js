890f8c9d1a1eb31ad33f0bb210d6141c18374a9a
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-15 09:52:27 +0200

    document events part 5 : onkeydown, onkeypress, onkeyup

--- main.js
+++ main.js
@@ -1,32 +1,12 @@
 /*
-	onclick
-	ondblclick
-	oncontextmenu
-	onmouseenter
-	onmouseleave
+	onkeydown
+	onkeypress
+	onkeyup
 */
 
-var myDiv = document.getElementById('test'),
+var myInput = document.getElementById('test'),
 	myNote = document.getElementById('note');
 
-myDiv.onclick = function () {
-	myNote.textContent = 'you clicked on me';
+myInput.onkeyup = function () {
+	myNote.textContent = myInput.value;
 };
-
-myDiv.ondblclick = function () {
-	myNote.textContent = 'you double clicked on me';
-};
-
-myDiv.oncontextmenu = function (e) {
-	e.preventDefault();
-	myNote.textContent = 'you can not right click here';
-};
-
-myDiv.onmouseenter = function () {
-	myNote.textContent = 'welcome';
-};
-
-myDiv.onmouseleave = function () {
-	myNote.textContent = 'good bye';
-};
-
