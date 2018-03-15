f36f814d62bc9b12845dcde7effa078eba71feaf
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-15 14:19:53 +0200

    print 3 numbers with while loop

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 
-var x = 1,
-	y = 1;
-while (x <= 12) {
-	while (y <= 12) {
-		console.log(x + ' * ' + y + ' = ' + x*y);
-		y += 1;
+var number1 = prompt('Enter your first number : '),
+	number2 = prompt('Enter your second number : '),
+	number3 = prompt('Enter your third number : ');
+
+function numbers(num1, num2, num3) {
+	var x = 0,
+		y = 0,
+		z = 0;
+	while (x <= num1) {
+		while (y <= num2) {
+			while (z <= num3) {
+				console.log(x, y, z);
+				z += 1;
+			}
+			z = 0;
+			y += 1;
+		}
+		y = 0;
+		x += 1;
 	}
-	console.log('*************');
-	y = 1;
-	x += 1;
 }
 
+numbers(parseInt(number1), parseInt(number2), parseInt(number3));
