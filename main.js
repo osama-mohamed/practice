74fa41da748a1a9f1e4c893dd5fe293994bc869c
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-15 13:34:31 +0200

    multiplication table with while loop

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
 
-var number1 = prompt('Enter your first number : '),
-	number2 = prompt('Enter your second number : '),
-	number3 = prompt('Enter your third number : ');
-
-function numbers(num1, num2, num3) {
-	var x ,
-		y,
-		z;
-	for(x = 0; x <= num1; x += 1) {
-		for(y = 0; y <= num2; y += 1) {
-			for(z = 0; z <= num3; z += 1) {
-				console.log(x, y, z);
-			}
-		}
+var x = 1,
+	y = 1;
+while (x <= 12) {
+	while (y <= 12) {
+		console.log(x + ' * ' + y + ' = ' + x*y);
+		y += 1;
 	}
+	console.log('*************');
+	y = 1;
+	x += 1;
 }
 
-numbers(parseInt(number1), parseInt(number2), parseInt(number3));
