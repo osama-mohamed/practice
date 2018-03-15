06d37f14e334731fd4ca1cbe20a3bb6b98c61fd9
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-15 11:06:04 +0200

    print 3 numbers with for loop

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-/*
-	onkeydown
-	onkeypress
-	onkeyup
-*/
 
-var myInput = document.getElementById('test'),
-	myNote = document.getElementById('note');
+var number1 = prompt('Enter your first number : '),
+	number2 = prompt('Enter your second number : '),
+	number3 = prompt('Enter your third number : ');
 
-myInput.onkeyup = function () {
-	myNote.textContent = myInput.value;
-};
+function numbers(num1, num2, num3) {
+	var x ,
+		y,
+		z;
+	for(x = 0; x <= num1; x += 1) {
+		for(y = 0; y <= num2; y += 1) {
+			for(z = 0; z <= num3; z += 1) {
+				console.log(x, y, z);
+			}
+		}
+	}
+}
+
+numbers(parseInt(number1), parseInt(number2), parseInt(number3));
