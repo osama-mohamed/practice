fe0ae2a2af4f1be6169c4b1c81c8fcdb2b9fe041
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-16 15:28:27 +0200

    set, map, weakset, weakmap

--- src/main.js
+++ src/main.js
@@ -1,20 +1,79 @@
 
-function sayHello($hello = 'Hello OSAMA MOHAMED') {
-	console.log($hello);
-}
+let myArray = [2, 4, 6, 8, 10],
+	mySet = new Set(myArray);
 
-sayHello();        // > returns the default value which is Hello OSAMA MOHAMED
-sayHello('Ahmed'); // > returns Ahmed
+mySet.add('12');
+mySet.add(20);
+mySet.add({a: 1, b: 'OSAMA'});
+mySet.delete(8);
+mySet.clear();
+
+console.log(mySet);
+console.log(mySet.size);
+
+mySet.forEach(function (val) {
+	console.log(val);
+});
 
 
+// -----------------------------------------------
 
 
+let myMap = new Map([['a', 'ahmed'], ['b', 'basma']]);
 
-let num1 = [1, 2, 3],
-	num2 = [4, 5, 6];
+myMap.set('s', 'salma');
+myMap.delete('a');
 
-function concatNums() {
-	console.log(num1 + ',' + num2);
-}
+console.log(myMap);
+console.log(myMap.size);
 
-concatNums(...num1, ...num2);
+
+// -----------------------------------------------
+
+
+let carWeakSet = new WeakSet();
+
+let car1 = {
+	name: 'honda',
+	model: 'civic'
+};
+
+let car2 = {
+	name: 'bmw',
+	model: 'f5'
+};
+
+carWeakSet.add(car1);
+carWeakSet.add(car2);
+carWeakSet.delete(car1);
+
+console.log(carWeakSet);
+
+// -----------------------------------------------
+
+let carWeakMap = new WeakMap();
+
+let key1 = {
+	id: 1
+};
+
+let c1 = {
+	name: 'honda',
+	model: 'civic'
+};
+
+let key2 = {
+	id: 2
+};
+
+let c2 = {
+	name: 'bmw',
+	model: 'f5'
+};
+
+carWeakMap.set(key1, c1);
+carWeakMap.set(key2, c2);
+
+carWeakMap.delete(key1);
+
+console.log(carWeakMap);
