7029fffae3c9ab624e0918017404ca1f6647a17c
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-16 14:44:12 +0200

    string methods and number methods

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
+/*
+	startsWith('string')  > in the first
+	endsWith('string')    > in the last
+	includes('string')    > from the first to the end
 
 
-let name = 'OSAMA';
+	isFinite()            > check for finite numbers only
+	isNaN()               > check for NaN
+	isInteger()           > check if number is an integer
+*/
 
-function makeUpperCase(word) {
-	return word.toUpperCase();
-}
+let text = `This is a text created by JavaScript`;
 
-let text = `<h1>${makeUpperCase('Hello')} ${name}</h1>
-			<p>This is a paragraph</p>`;
+console.log(text.startsWith('This'));
+console.log(text.endsWith('JavaScript'));
+console.log(text.includes('created'));
 
-document.getElementById('test').innerHTML = text;
+
+console.log(Number.isFinite(-6));
+console.log(Number.isFinite(6));
+console.log(Number.isFinite(6.66));
+console.log(Number.isFinite(Infinity));
+console.log(Number.isFinite(NaN));
+
+
+console.log(Number.isNaN(NaN));
+
+
+console.log(Number.isInteger(6));
+console.log(Number.isInteger(-6));
+console.log(Number.isInteger(6.66));
+console.log(Number.isInteger(Infinity));
+console.log(Number.isInteger(NaN));
