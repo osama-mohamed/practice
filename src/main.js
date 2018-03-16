f11bb0d3aac29d604c83053063d6cc7fcd8c537e
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-16 14:57:32 +0200

    default params and spread operators

--- src/main.js
+++ src/main.js
@@ -1,33 +1,20 @@
-/*
-	startsWith('string')  > in the first
-	endsWith('string')    > in the last
-	includes('string')    > from the first to the end
+
+function sayHello($hello = 'Hello OSAMA MOHAMED') {
+	console.log($hello);
+}
+
+sayHello();        // > returns the default value which is Hello OSAMA MOHAMED
+sayHello('Ahmed'); // > returns Ahmed
 
 
-	isFinite()            > check for finite numbers only
-	isNaN()               > check for NaN
-	isInteger()           > check if number is an integer
-*/
-
-let text = `This is a text created by JavaScript`;
-
-console.log(text.startsWith('This'));
-console.log(text.endsWith('JavaScript'));
-console.log(text.includes('created'));
 
 
-console.log(Number.isFinite(-6));
-console.log(Number.isFinite(6));
-console.log(Number.isFinite(6.66));
-console.log(Number.isFinite(Infinity));
-console.log(Number.isFinite(NaN));
 
+let num1 = [1, 2, 3],
+	num2 = [4, 5, 6];
 
-console.log(Number.isNaN(NaN));
+function concatNums() {
+	console.log(num1 + ',' + num2);
+}
 
-
-console.log(Number.isInteger(6));
-console.log(Number.isInteger(-6));
-console.log(Number.isInteger(6.66));
-console.log(Number.isInteger(Infinity));
-console.log(Number.isInteger(NaN));
+concatNums(...num1, ...num2);
