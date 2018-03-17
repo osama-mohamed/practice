cdde16f59332d5399520b6aee91295c98731bea5
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-17 09:25:45 +0200

    playing sound when keydown

--- dist/main.js
+++ dist/main.js
@@ -1,68 +1,22 @@
 'use strict';
 
-var _marked = /*#__PURE__*/regeneratorRuntime.mark(g1);
-
-function g1() {
-	return regeneratorRuntime.wrap(function g1$(_context) {
-		while (1) {
-			switch (_context.prev = _context.next) {
-				case 0:
-					console.log('Hello');
-					_context.next = 3;
-					return 'Yield 1 Started !';
-
-				case 3:
-					console.log('World');
-					_context.next = 6;
-					return 'Yield 2 Started !';
-
-				case 6:
-					return _context.abrupt('return', 'Return');
-
-				case 7:
-				case 'end':
-					return _context.stop();
-			}
-		}
-	}, _marked, this);
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return;
+	e.target.classList.remove('playing');
 }
 
-var g = g1();
+function playSound(e) {
+	var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+	var key = document.querySelector('div[data-key="' + e.keyCode + '"]');
+	if (!audio) return;
 
-console.log(g.next()); // first yield & done = false
-console.log(g.next()); // second yield & done = false
-console.log(g.next()); // return & done = true
-
-
-/*
-console.log(g.next().value);
-console.log(g.next().value);
-console.log(g.next().value);
-*/
-
-console.log('**********');
-
-var _iteratorNormalCompletion = true;
-var _didIteratorError = false;
-var _iteratorError = undefined;
-
-try {
-	for (var _iterator = g[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-		var val = _step.value;
-
-		console.log(val);
-	}
-} catch (err) {
-	_didIteratorError = true;
-	_iteratorError = err;
-} finally {
-	try {
-		if (!_iteratorNormalCompletion && _iterator.return) {
-			_iterator.return();
-		}
-	} finally {
-		if (_didIteratorError) {
-			throw _iteratorError;
-		}
-	}
+	key.classList.add('playing');
+	audio.currentTime = 0;
+	audio.play();
 }
+
+var keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(function (key) {
+	return key.addEventListener('transitionend', removeTransition);
+});
+window.addEventListener('keydown', playSound);
--- src/main.js
+++ src/main.js
@@ -1,27 +1,18 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+		e.target.classList.remove('playing');
+	}
 
-function *g1() {
-	console.log('Hello');
-	yield 'Yield 1 Started !';
-	console.log('World');
-	yield 'Yield 2 Started !';
-	return 'Return';
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+	if (!audio) return;
+
+	key.classList.add('playing');
+	audio.currentTime = 0;
+	audio.play();
 }
 
-var g = g1();
-
-console.log(g.next());  // first yield & done = false
-console.log(g.next());  // second yield & done = false
-console.log(g.next());  // return & done = true
-
-
-/*
-console.log(g.next().value);
-console.log(g.next().value);
-console.log(g.next().value);
-*/
-
-console.log('**********');
-
-for(let val of g) {
-	console.log(val);
-}
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
