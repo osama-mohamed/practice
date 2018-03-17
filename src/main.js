9424a37297e84f4ea1e6b849c965a9d5fdeb09ed
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-17 07:51:46 +0200

    promises

--- src/main.js
+++ src/main.js
@@ -1,47 +1,57 @@
-/*
-function Prefixer(prefix) {
-	this.prefix = prefix;
+
+var myPromise = Promise.resolve('Yeah');
+myPromise.then((res) => console.log(res));
+// myPromise.then(function(res) {console.log(res)});
+
+
+var myPromise2 = new Promise(function (resolve, reject) {
+	console.log('Wait 2 seconds!');
+	setTimeout(() => resolve(4), 2000);
+});
+
+myPromise2.then((res) => {
+	res += 3;
+	console.log(res);
+});
+
+
+function getData(method, url) {
+	return new Promise(function (resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.onload = function () {
+			if (this.status >= 200 && this.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject({
+					status: this.status,
+					statusText: xhr.statusText
+				});
+			}
+		};
+		xhr.onerror = function () {
+			reject({
+				status: this.status,
+				statusText: xhr.statusText
+			});
+		};
+		xhr.send();
+	});
 }
 
-Prefixer.prototype.prefixArray = function (arr) {
-	let that = this;
-	return arr.map(function (x) {
-		console.log(that.prefix + x);
-	});
-};
-
-let pre = new Prefixer('Hello ');
-pre.prefixArray(['OSAMA', 'ESLAM']);
-*/
-
-function Prefixer(prefix) {
-	this.prefix = prefix;
-}
-
-Prefixer.prototype.prefixArray = function (arr) {
-	return arr.map((x) => {
-		console.log(this.prefix + x);
-	});
-};
-
-let pre = new Prefixer('Hello ');
-pre.prefixArray(['OSAMA', 'ESLAM']);
-
-
-/*
-let add = function (a, b) {
-	let sum = a + b;
-	console.log(sum);
-	return false;
-};
-add(4, 6);
-*/
-
-
-
-let add = (a, b) => {
-	let sum = a + b;
-	console.log(sum);
-	return false;
-};
-add(4, 6);
+getData('GET', 'https://api.github.com/users').then(function (data) {
+	console.log(data);
+	let todos = JSON.parse(data);
+	let output = '';
+	for (let todo of todos) {
+		output += `
+			<div>
+				<h3>Username : ${todo.login}</h3>
+				<p>URL : <a href="${todo.url}" target="_blank">${todo.url}</a></p>
+			</div>
+		`;
+	}
+	document.getElementById('test').innerHTML = output;
+}).catch(function (err) {
+	console.log(err);
+});
