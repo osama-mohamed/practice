ad2a637ccf0999e3f8df96699d286358187d2c84
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-17 11:35:51 +0200

    playing with css variables and javascript

--- dist/main.js
+++ dist/main.js
@@ -1,22 +1,15 @@
 'use strict';
 
-function removeTransition(e) {
-	if (e.propertyName !== 'transform') return;
-	e.target.classList.remove('playing');
+var inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+	var suffix = this.dataset.sizing || '';
+	document.documentElement.style.setProperty('--' + this.name, this.value + suffix);
 }
 
-function playSound(e) {
-	var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
-	var key = document.querySelector('div[data-key="' + e.keyCode + '"]');
-	if (!audio) return;
-
-	key.classList.add('playing');
-	audio.currentTime = 0;
-	audio.play();
-}
-
-var keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(function (key) {
-	return key.addEventListener('transitionend', removeTransition);
+inputs.forEach(function (input) {
+	return input.addEventListener('change', handleUpdate);
 });
-window.addEventListener('keydown', playSound);
+inputs.forEach(function (input) {
+	return input.addEventListener('mousemove', handleUpdate);
+});
--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
-const secondHand = document.querySelector('.second-hand');
-const minHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+const inputs = document.querySelectorAll('.controls input');
 
-function setDate() {
-const now = new Date();
-
-const seconds = now.getSeconds();
-const secondsDegrees = ((seconds / 60) * 360) + 90;
-secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-const mins = now.getMinutes();
-const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-minHand.style.transform = `rotate(${minsDegrees}deg)`;
-
-const hour = now.getHours();
-const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+function handleUpdate() {
+	const suffix = this.dataset.sizing || '';
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-setInterval(setDate, 1000);
-
-setDate();
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
