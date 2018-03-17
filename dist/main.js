fdcf9e6cfca03f38739fac2616881168d038aa58
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-17 08:08:53 +0200

    generators

--- dist/main.js
+++ dist/main.js
@@ -1,24 +1,68 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _marked = /*#__PURE__*/regeneratorRuntime.mark(g1);
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function g1() {
+	return regeneratorRuntime.wrap(function g1$(_context) {
+		while (1) {
+			switch (_context.prev = _context.next) {
+				case 0:
+					console.log('Hello');
+					_context.next = 3;
+					return 'Yield 1 Started !';
 
-var o = 'osama';
+				case 3:
+					console.log('World');
+					_context.next = 6;
+					return 'Yield 2 Started !';
 
-console.log(o);
+				case 6:
+					return _context.abrupt('return', 'Return');
 
-var User = function () {
-	function User() {
-		_classCallCheck(this, User);
-	}
-
-	_createClass(User, [{
-		key: 'register',
-		value: function register() {
-			console.log('user registered!');
+				case 7:
+				case 'end':
+					return _context.stop();
+			}
 		}
-	}]);
+	}, _marked, this);
+}
 
-	return User;
-}();
+var g = g1();
+
+console.log(g.next()); // first yield & done = false
+console.log(g.next()); // second yield & done = false
+console.log(g.next()); // return & done = true
+
+
+/*
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+*/
+
+console.log('**********');
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+	for (var _iterator = g[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+		var val = _step.value;
+
+		console.log(val);
+	}
+} catch (err) {
+	_didIteratorError = true;
+	_iteratorError = err;
+} finally {
+	try {
+		if (!_iteratorNormalCompletion && _iterator.return) {
+			_iterator.return();
+		}
+	} finally {
+		if (_didIteratorError) {
+			throw _iteratorError;
+		}
+	}
+}
--- src/main.js
+++ src/main.js
@@ -1,57 +1,27 @@
 
-var myPromise = Promise.resolve('Yeah');
-myPromise.then((res) => console.log(res));
-// myPromise.then(function(res) {console.log(res)});
-
-
-var myPromise2 = new Promise(function (resolve, reject) {
-	console.log('Wait 2 seconds!');
-	setTimeout(() => resolve(4), 2000);
-});
-
-myPromise2.then((res) => {
-	res += 3;
-	console.log(res);
-});
-
-
-function getData(method, url) {
-	return new Promise(function (resolve, reject) {
-		var xhr = new XMLHttpRequest();
-		xhr.open(method, url);
-		xhr.onload = function () {
-			if (this.status >= 200 && this.status < 300) {
-				resolve(xhr.response);
-			} else {
-				reject({
-					status: this.status,
-					statusText: xhr.statusText
-				});
-			}
-		};
-		xhr.onerror = function () {
-			reject({
-				status: this.status,
-				statusText: xhr.statusText
-			});
-		};
-		xhr.send();
-	});
+function *g1() {
+	console.log('Hello');
+	yield 'Yield 1 Started !';
+	console.log('World');
+	yield 'Yield 2 Started !';
+	return 'Return';
 }
 
-getData('GET', 'https://api.github.com/users').then(function (data) {
-	console.log(data);
-	let todos = JSON.parse(data);
-	let output = '';
-	for (let todo of todos) {
-		output += `
-			<div>
-				<h3>Username : ${todo.login}</h3>
-				<p>URL : <a href="${todo.url}" target="_blank">${todo.url}</a></p>
-			</div>
-		`;
-	}
-	document.getElementById('test').innerHTML = output;
-}).catch(function (err) {
-	console.log(err);
-});
+var g = g1();
+
+console.log(g.next());  // first yield & done = false
+console.log(g.next());  // second yield & done = false
+console.log(g.next());  // return & done = true
+
+
+/*
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+*/
+
+console.log('**********');
+
+for(let val of g) {
+	console.log(val);
+}
