f7bed683d5312d98bb503e05e856df8a8586d85a
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-17 14:59:16 +0200

    array part 2

--- dist/main.js
+++ dist/main.js
@@ -2,41 +2,37 @@
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-var endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+var people = [{ name: 'Wes', year: 1988 }, { name: 'Kait', year: 1986 }, { name: 'Irv', year: 1970 }, { name: 'Lux', year: 2015 }];
 
-var cities = [];
-fetch(endpoint).then(function (blob) {
-  return blob.json();
-}).then(function (data) {
-  return cities.push.apply(cities, _toConsumableArray(data));
+var comments = [{ text: 'Love this!', id: 523423 }, { text: 'Super good', id: 823423 }, { text: 'You are the best', id: 2039842 }, { text: 'Ramen is my fav food ever', id: 123523 }, { text: 'Nice Nice Nice!', id: 542328 }];
+
+// const isAdult = people.some(function(person) {
+//   const currentYear = (new Date()).getFullYear();
+//   if(currentYear - person.year >= 19) {
+//     return true;
+//   }
+// });
+
+var isAdult = people.some(function (person) {
+  return new Date().getFullYear() - person.year >= 19;
 });
+console.log({ isAdult: isAdult });
 
-function findMatches(wordToMatch, cities) {
-  return cities.filter(function (place) {
-    var regex = new RegExp(wordToMatch, 'gi');
-    return place.city.match(regex) || place.state.match(regex);
-  });
-}
+var allAdults = people.every(function (person) {
+  return new Date().getFullYear() - person.year >= 19;
+});
+console.log({ allAdults: allAdults });
 
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+var comment = comments.find(function (comment) {
+  return comment.id === 823423;
+});
+console.log(comment);
 
-function displayMatches() {
-  var _this = this;
+var index = comments.findIndex(function (comment) {
+  return comment.id === 823423;
+});
+console.log(index);
 
-  var matchArray = findMatches(this.value, cities);
-  var html = matchArray.map(function (place) {
-    var regex = new RegExp(_this.value, 'gi');
-    var cityName = place.city.replace(regex, '<span class="hl">' + _this.value + '</span>');
-    var stateName = place.state.replace(regex, '<span class="hl">' + _this.value + '</span>');
-    return '\n      <li>\n        <span class="name">' + cityName + ', ' + stateName + '</span>\n        <span class="population">' + numberWithCommas(place.population) + '</span>\n      </li>\n    ';
-  }).join('');
-  suggestions.innerHTML = html;
-}
+// comments.splice(index, 1);
 
-var searchInput = document.querySelector('.search');
-var suggestions = document.querySelector('.suggestions');
-
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+var newComments = [].concat(_toConsumableArray(comments.slice(0, index)), _toConsumableArray(comments.slice(index + 1)));
--- src/main.js
+++ src/main.js
@@ -1,39 +1,44 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const cities = [];
-fetch(endpoint)
-  .then(blob => blob.json())
-  .then(data => cities.push(...data));
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 },
+];
 
-function findMatches(wordToMatch, cities) {
-  return cities.filter(place => {
-    const regex = new RegExp(wordToMatch, 'gi');
-    return place.city.match(regex) || place.state.match(regex)
-  });
-}
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
 
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
 
-function displayMatches() {
-  const matchArray = findMatches(this.value, cities);
-  const html = matchArray.map(place => {
-    const regex = new RegExp(this.value, 'gi');
-    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-    return `
-      <li>
-        <span class="name">${cityName}, ${stateName}</span>
-        <span class="population">${numberWithCommas(place.population)}</span>
-      </li>
-    `;
-  }).join('');
-  suggestions.innerHTML = html;
-}
+// const isAdult = people.some(function(person) {
+//   const currentYear = (new Date()).getFullYear();
+//   if(currentYear - person.year >= 19) {
+//     return true;
+//   }
+// });
 
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
+const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+console.log({isAdult});
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+console.log({allAdults});
+
+
+const comment = comments.find(comment => comment.id === 823423);
+console.log(comment);
+
+
+const index = comments.findIndex(comment => comment.id === 823423);
+console.log(index);
+
+// comments.splice(index, 1);
+
+const newComments = [
+  ...comments.slice(0, index),
+  ...comments.slice(index + 1)
+];
