1940ebaa0b3beb1e03b6ffd7210244e22ad10224
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-18 15:37:30 +0200

    slide in images on page scroll

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,38 @@
 'use strict';
 
-var pressed = [];
-var secretCode = 'osama';
+function debounce(func) {
+		var wait = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 20;
+		var immediate = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
 
-window.addEventListener('keyup', function (e) {
-  pressed.push(e.key);
-  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-  if (pressed.join('').includes(secretCode)) {
-    cornify_add();
-  }
-});
+		var timeout;
+		return function () {
+				var context = this,
+				    args = arguments;
+				var later = function later() {
+						timeout = null;
+						if (!immediate) func.apply(context, args);
+				};
+				var callNow = immediate && !timeout;
+				clearTimeout(timeout);
+				timeout = setTimeout(later, wait);
+				if (callNow) func.apply(context, args);
+		};
+}
+
+var sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+		sliderImages.forEach(function (sliderImage) {
+				var slideInAt = window.scrollY + window.innerHeight - sliderImage.height / 2;
+				var imageBottom = sliderImage.offsetTop + sliderImage.height;
+				var isHalfShown = slideInAt > sliderImage.offsetTop;
+				var isNotScrolledPast = window.scrollY < imageBottom;
+				if (isHalfShown && isNotScrolledPast) {
+						sliderImage.classList.add('active');
+				} else {
+						sliderImage.classList.remove('active');
+				}
+		});
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
-const pressed = [];
-const secretCode = 'osama';
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+	var context = this, args = arguments;
+	var later = function() {
+	  timeout = null;
+	  if (!immediate) func.apply(context, args);
+	};
+	var callNow = immediate && !timeout;
+	clearTimeout(timeout);
+	timeout = setTimeout(later, wait);
+	if (callNow) func.apply(context, args);
+  };
+}
 
-window.addEventListener('keyup', (e) => {
-  pressed.push(e.key);
-  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-  if (pressed.join('').includes(secretCode)) {
-    cornify_add();
-  }
-});
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+  sliderImages.forEach(sliderImage => {
+	const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+	const imageBottom = sliderImage.offsetTop + sliderImage.height;
+	const isHalfShown = slideInAt > sliderImage.offsetTop;
+	const isNotScrolledPast = window.scrollY < imageBottom;
+	if (isHalfShown && isNotScrolledPast) {
+	  sliderImage.classList.add('active');
+	} else {
+	  sliderImage.classList.remove('active');
+	}
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
