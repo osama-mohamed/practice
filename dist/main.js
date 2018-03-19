46ea1ec05a3827cf71269ca1abeb833af6b4b30e
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-20 00:19:43 +0200

    voice recognition : speak to write in english

--- dist/main.js
+++ dist/main.js
@@ -1,94 +1,32 @@
 'use strict';
 
-var video = document.querySelector('.player');
-var canvas = document.querySelector('.photo');
-var ctx = canvas.getContext('2d');
-var strip = document.querySelector('.strip');
-var snap = document.querySelector('.snap');
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-function getVideo() {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(function (localMediaStream) {
-    video.src = window.URL.createObjectURL(localMediaStream);
-    video.play();
-  }).catch(function (err) {
-    console.error('OH NO!!!', err);
-  });
-}
+var recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
 
-function paintToCanvas() {
-  var width = video.videoWidth;
-  var height = video.videoHeight;
-  canvas.width = width;
-  canvas.height = height;
+var p = document.createElement('p');
+var words = document.querySelector('.words');
+words.appendChild(p);
 
-  return setInterval(function () {
-    ctx.drawImage(video, 0, 0, width, height);
-    // take the pixels out
-    var pixels = ctx.getImageData(0, 0, width, height);
+recognition.addEventListener('result', function (e) {
+  var transcript = Array.from(e.results).map(function (result) {
+    return result[0];
+  }).map(function (result) {
+    return result.transcript;
+  }).join('');
 
-    // pixels = redEffect(pixels);
-    // pixels = rgbSplit(pixels);
-    // ctx.globalAlpha = 0.8;
-    // pixels = greenScreen(pixels);
+  // const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+  // p.textContent = poopScript;
+  p.textContent = transcript;
 
-    ctx.putImageData(pixels, 0, 0);
-  }, 16);
-}
-
-function takePhoto() {
-  // played the sound
-  snap.currentTime = 0;
-  snap.play();
-
-  // take the data out of the canvas
-  var data = canvas.toDataURL('image/jpeg');
-  var link = document.createElement('a');
-  link.href = data;
-  var picDate = new Date();
-  link.setAttribute('download', picDate);
-  link.innerHTML = '<img src="' + data + '" alt="' + picDate + '" />';
-  strip.insertBefore(link, strip.firsChild);
-}
-
-function redEffect(pixels) {
-  for (var i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
   }
-  return pixels;
-}
+});
 
-function rgbSplit(pixels) {
-  for (var i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; // RED
-    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
-    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
-  }
-  return pixels;
-}
+recognition.addEventListener('end', recognition.start);
 
-function greenScreen(pixels) {
-  var levels = {};
-
-  document.querySelectorAll('.rgb input').forEach(function (input) {
-    levels[input.name] = input.value;
-  });
-
-  for (var i = 0; i < pixels.data.length; i = i + 4) {
-    red = pixels.data[i + 0];
-    green = pixels.data[i + 1];
-    blue = pixels.data[i + 2];
-    alpha = pixels.data[i + 3];
-
-    if (red >= levels.rmin && green >= levels.gmin && blue >= levels.bmin && red <= levels.rmax && green <= levels.gmax && blue <= levels.bmax) {
-      // take it out!
-      pixels.data[i + 3] = 0;
-    }
-  }
-  return pixels;
-}
-
-getVideo();
-
-video.addEventListener('canplay', paintToCanvas);
+recognition.start();
