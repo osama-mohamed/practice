db9e12c70b82d9ce612a23815bee16731a0ccc2f
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-20 09:13:32 +0200

    follow and highlight all links in the page

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,22 @@
 'use strict';
 
-var arrow = document.querySelector('.arrow');
-var speed = document.querySelector('.speed-value');
+var triggers = document.querySelectorAll('a');
+var highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.appendChild(highlight);
 
-navigator.geolocation.watchPosition(function (data) {
-  console.log(data);
-  speed.textContent = data.coords.speed;
-  arrow.style.transform = 'rotate(' + data.coords.heading + 'deg)';
-}, function (err) {
-  console.error(err);
+function highlightLink() {
+  var linkCoords = this.getBoundingClientRect();
+  var coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  };
+  highlight.style.width = coords.width + 'px';
+  highlight.style.height = coords.height + 'px';
+  highlight.style.transform = 'translate(' + coords.left + 'px, ' + coords.top + 'px)';
+}
+triggers.forEach(function (a) {
+  return a.addEventListener('mouseenter', highlightLink);
 });
