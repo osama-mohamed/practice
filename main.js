93d0431ad9c1940b1999ad9ce9c7481fdce58f78
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-24 23:07:55 +0200

    data binding

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ new Vue({
 	el: '#vue-app',
 	data: {
 		name: 'OSAMA',
-		job: 'Python web Developer'
+		job: 'Python web Developer',
+		facebook: 'https://www.facebook.com/osama.mohamed.ms',
+		facebookTag: '<a href="https://www.facebook.com/osama.mohamed.ms">OSAMA MOHAMED</a>'
 	},
 	methods: {
 		greet: function (name) {
