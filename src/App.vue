1575579c97fc7bd1506822fcec308104dfaf8afc
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-25 19:05:49 +0200

    component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <p>{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'This is title'
+    }
+  },
+  methods: {
+    greeting: function () {
+       return 'Hello Visitor'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
