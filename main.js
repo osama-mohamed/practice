d98e416eee351230919054d5a81818f27a361bf4
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-25 16:49:46 +0200

    multiple vue instances

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
-new Vue({
-	el: '#vue-app',
+let one = new Vue({
+	el: '#vue-app-one',
 	data: {
-		health: 100,
-        ended: false
+		title: 'Vue component one'
 	},
 	methods: {
-		punch: function(){
-            this.health -= 10;
-            if(this.health <= 0){
-                this.ended = true;
-            }
-        },
-        restart: function(){
-            this.health = 100;
-            this.ended = false;
-        }
 	},
 	computed: {
+		greet: function () {
+			return 'Hello from vue app one'
+		}
+	}
+});
 
+
+let two = new Vue({
+	el: '#vue-app-two',
+	data: {
+		title: 'Vue component two'
+	},
+	methods: {
+		changeTitle: function () {
+			one.title = 'title has been changed from vue app two'
+		}
+	},
+	computed: {
+		greet: function () {
+			return 'Hello from vue app two'
+		}
 	}
 });
