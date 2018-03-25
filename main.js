253aa91585c57f391cc7c354bb9af54201b512a0
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-25 17:22:04 +0200

    intro to components

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
+/*
+const data = {	// change all elements
+	name: 'OSAMA'
+}
+*/
+
+Vue.component('greeting', {
+	template: `<p>Hello i'm {{name}}. <button v-on:click="changeName">Change Name</button></p>`,
+	data: function () {
+		return {			// change one element only
+			name: 'OSAMA'
+		}
+	},
+	/*
+	data: function () {
+		return data			// change all elements
+	},
+	*/
+	methods: {
+		changeName: function () {
+			this.name = 'OSAMA MOHAMED'
+		}
+	}
+});
+
 let one = new Vue({
 	el: '#vue-app-one',
 	data: {
-		title: 'Vue component one'
+
 	},
 	methods: {
 	},
 	computed: {
-		greet: function () {
-			return 'Hello from vue app one'
-		}
+
 	}
 });
 
@@ -16,16 +39,12 @@ let one = new Vue({
 let two = new Vue({
 	el: '#vue-app-two',
 	data: {
-		title: 'Vue component two'
+
 	},
 	methods: {
-		changeTitle: function () {
-			one.title = 'title has been changed from vue app two'
-		}
+
 	},
 	computed: {
-		greet: function () {
-			return 'Hello from vue app two'
-		}
+
 	}
 });
