4b46ae6af440236980c8705b493a7f40cc4e3337
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-25 10:15:48 +0200

    dynamic classes

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
 new Vue({
 	el: '#vue-app',
 	data: {
-		age: 20,
-		a: 0,
-		b: 0
+		os: true,
+		available: false,
+		nearby: true
 	},
 	methods: {
-		/*addToA: function () {
-			console.log('add to a')
-			return this.a + this.age
-		},
-		addToB: function () {
-			console.log('add to b')
-			return this.b + this.age
-		}*/
+
 	},
 	computed: {
-		addToA: function () {
-			console.log('add to a')
-			return this.a + this.age
-		},
-		addToB: function () {
-			console.log('add to b')
-			return this.b + this.age
+		compClasses: function () {
+			return {
+				available: this.available,
+				nearby: this.nearby
+			}
 		}
 	}
 });
