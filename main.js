d9d6daacbde3e81c9bf14feb5fe95f29045c9cbc
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-25 08:20:29 +0200

    events

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 new Vue({
 	el: '#vue-app',
 	data: {
-		name: 'OSAMA',
-		job: 'Python web Developer',
-		facebook: 'https://www.facebook.com/osama.mohamed.ms',
-		facebookTag: '<a href="https://www.facebook.com/osama.mohamed.ms">OSAMA MOHAMED</a>'
+		age: 23,
+		x: 0,
+		y: 0
 	},
 	methods: {
-		greet: function (name) {
-			return 'Hello ' + name + ' This if function'
+		add: function () {
+			this.age += 1
+		},
+		sub: function () {
+			this.age -= 1
+		},
+		updateXY: function (e) {
+			this.x = e.offsetX,
+			this.y = e.offsetY
 		}
 	}
 });
