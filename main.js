7a80c28229b6a0dd2de84ae5f728208b4027ed05
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-25 17:42:11 +0200

    refs

--- main.js
+++ main.js
@@ -1,48 +1,13 @@
-/*
-const data = {	// change all elements
-	name: 'OSAMA'
-}
-*/
-
-Vue.component('greeting', {
-	template: `<p>Hello i'm {{name}}. <button v-on:click="changeName">Change Name</button></p>`,
-	data: function () {
-		return {			// change one element only
-			name: 'OSAMA'
-		}
-	},
-	/*
-	data: function () {
-		return data			// change all elements
-	},
-	*/
-	methods: {
-		changeName: function () {
-			this.name = 'OSAMA MOHAMED'
-		}
-	}
-});
-
-let one = new Vue({
-	el: '#vue-app-one',
+new Vue({
+	el: '#vue-app',
 	data: {
-
+		output: ''
 	},
 	methods: {
-	},
-	computed: {
-
-	}
-});
-
-
-let two = new Vue({
-	el: '#vue-app-two',
-	data: {
-
-	},
-	methods: {
-
+		readRefs: function () {
+			console.log(this.$refs.test.innerText);
+			this.output = this.$refs.inputRef.value
+		}
 	},
 	computed: {
 
