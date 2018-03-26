95c2105b4cfdf7790213ee19eb677cabea0a9908
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-26 18:35:13 +0200

    input preview

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
    <div>
-      <keep-alive>    <!-- keep-alive > to keep data into components -->
-        <component v-bind:is="component"></component> <!-- component > to switch between components -->
-      </keep-alive>
-      <button v-on:click="component = 'form-one'">Show form one</button>
-      <button v-on:click="component = 'form-two'">Show form two</button>
+      <addBlog></addBlog>
    </div>
 </template>
 
 <script>
-import formOne from './components/formOne.vue'
-import formTwo from './components/formTwo.vue'
-
+import addBlog from './components/addBlog'
 
 export default {
   components: {
-    'form-one': formOne,
-    'form-two': formTwo
+    'addBlog': addBlog
   },
   data () {
     return {
-      component: 'form-one'
     }
   },
   methods: {
--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 
 
-export const bus = new Vue();
-
-
 new Vue({
   el: '#app',
   render: h => h(App)
