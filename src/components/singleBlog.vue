d80d7fb33efbd04074767cd8134b023badf60884
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-27 14:44:38 +0200

    pagination

--- src/components/singleBlog.vue
+++ src/components/singleBlog.vue
@@ -1,30 +1,64 @@
 <template>
   <div id="single-blog">
-    <h1>{{blog.title}}</h1>
-    <article>{{blog.body}}</article>
+    <template v-if="!loading">
+      <h1>{{blog.title}}</h1>
+      <article>{{blog.body}}</article>
+    </template>
+    <p v-else>loading...</p>
+    <br>
+    <br>
+    <button v-on:click="prev()" v-if="id > 1">Previous</button>
+    <button v-on:click="next()" v-if="id < 100">Next</button>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      id: this.$route.params.id,
-      blog: {}
-    }
+      id: parseInt(this.$route.params.id),
+      blog: {},
+      loading: false
+    };
   },
-  created () {
-    this.$http.get('https://jsonplaceholder.typicode.com/posts/' + this.id).then(function (data) {
-      this.blog = data.body;
-    });
+  created() {
+    this.getData();
+  },
+  methods: {
+    next: function() {
+      if (this.id <= 100) {
+        this.id += 1;
+      }
+      this.$router.push(`/blog/${this.id}`);
+      this.getData();
+    },
+    prev: function() {
+      if (this.id > 1) {
+        this.id -= 1;
+      }
+      this.$router.push(`/blog/${this.id}`);
+      this.getData();
+    },
+    getData: function() {
+      this.loading = true;
+      this.$http
+        .get(`https://jsonplaceholder.typicode.com/posts/${this.id}`)
+        .then(data => {
+          this.blog = data.body;
+          this.loading = false;
+        })
+        .catch(err => {
+          this.loading = false;
+          console.log(err);
+        });
+    }
   }
-
-}
+};
 </script>
 
 <style scoped>
-#single-blog{
-    max-width: 960px;
-    margin: 0 auto;
+#single-blog {
+  max-width: 960px;
+  margin: 0 auto;
 }
 </style>
