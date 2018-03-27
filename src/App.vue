8e9fb58585a229f9103db72d2a3837438f435af5
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-27 10:17:00 +0200

    add router links and exact attribute

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
    <div>
+     <app-header></app-header>
      <router-view></router-view>
    </div>
 </template>
 
 <script>
+import header from './components/header'
 import addBlog from './components/addBlog'
 import showBlogs from './components/showBlogs'
 import listBlogs from './components/listBlogs'
 
 export default {
   components: {
+    'app-header': header,
     'addBlog': addBlog,
     'showBlogs': showBlogs,
     'listBlogs': listBlogs
