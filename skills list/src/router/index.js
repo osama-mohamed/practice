c094313a6e9f9e1cd3c3cbc5e4d036461bf8b497
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-31 23:51:56 +0200

    skills list

---
+++ skills list/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Skills from '@/components/Skills'
+import About from '@/components/About'
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {
+      path: '/',
+      name: 'Skills',
+      component: Skills
+    },
+    {
+      path: '/about/:name',
+      name: 'About',
+      component: About
+    }
+  ]
+})
