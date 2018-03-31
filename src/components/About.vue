77bb56f2389a14f70addafe3557e7f4aae749b03
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-03-31 12:52:31 +0200

    skills list

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,25 +1,17 @@
 <template>
   <div class="about">
-    <h1 class="page-header">About</h1>
-    <p>This is a customer manager app built with the Vue.js framework</p>
-    <br>
-    <br>
-    <br>
-    <h1 class="text-center">Developed By : <a href="https://www.facebook.com/osama.mohamed.ms">OSAMA MOHAMED</a></h1>
+    <h1>Hello {{ their_name }}, this is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'about',
-  data () {
+  name: 'Skills',
+  data() {
     return {
-
+      their_name: this.$route.params.name
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -2,63 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 
-import VueRouter from 'vue-router'
-import vueResource from 'vue-resource'
-import Customers from './components/Customers'
-import CustomerDetails from './components/CustomerDetails'
-import About from './components/About'
-import Add from './components/Add'
-import Edit from './components/Edit'
+import VeeValidate from 'vee-validate'
 
-Vue.use(VueRouter);
-Vue.use(vueResource);
+Vue.use(VeeValidate);
 
-const router = new VueRouter({
-  mode: 'history',
-  base: __dirname,
-  routes: [
-    {path: '/', name: 'home', component: Customers},
-    {path: '/about', component: About},
-    {path: '/add', component: Add},
-    {path: '/customer/:id', name: 'detail', component: CustomerDetails},
-    {path: '/customer/edit/:id', name: 'update', component: Edit},
-  ]
-});
-
-// Vue.config.productionTip = false
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
-  template: `
-    <div id="app">      
-      <!--<nav class="navbar navbar-inverse navbar-fixed-top">-->
-      <nav class="navbar navbar-default">
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Customers</a>
-          </div>
-          <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-              <li><router-link to="/">Home</router-link></li>
-              <li><router-link to="/about">About</router-link></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-              <li><router-link to="/add">Add Customer</router-link></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </nav>
-      <div class="container">
-        <router-view></router-view>
-      </div>
-    </div>
-  `
-}).$mount('#app');
+  components: { App },
+  template: '<App/>'
+})
