5aa7f7db22420e393f17c506b03faa735db08d33
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-04-01 11:12:02 +0200

    install and config

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDqBxo82epuwztS2hKQ-jh6dzImDMJL2jU",
+  authDomain: "vue-osama.firebaseapp.com",
+  databaseURL: "https://vue-osama.firebaseio.com",
+  projectId: "vue-osama",
+  storageBucket: "vue-osama.appspot.com",
+  messagingSenderId: "465272663405"
+}
