b83259d1475eab286bd9f15fec24f408dfd28bf4
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-04-03 01:14:58 +0200

    create components

---
+++ src/components/Articles.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    articles
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'articles',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
