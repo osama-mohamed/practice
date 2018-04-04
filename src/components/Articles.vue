ce88640907eec8d89edcccbce3302727002b589b
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-04-04 10:50:20 +0200

    fix toggle

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -11,8 +11,8 @@
       </div>
       <button type="submit" class="btn btn-success btn-block" v-if="!update">Add</button>
       <button type="submit" class="btn btn-success btn-block update" v-if="update">{{update}}</button>
+      <button type="button" v-if="update" @click="changeToAdd" class="btn btn-info btn-block mb-4">Click to change to add mode</button>
     </form>
-    <button v-if="update" @click="changeToAdd" class="btn btn-info btn-block mb-4">Click to change to add mode</button>
     <h2 class="text-center mb-4">Articles</h2>
     <nav aria-label="Page navigation example">
       <ul class="pagination">
@@ -126,40 +126,44 @@ export default {
       }*/
     },
     addArticle () {
-      if (this.edit === false) {
-        fetch('http://localhost:8000/articles-api/new/', {
-          method: 'post',
-          body: JSON.stringify(this.article),
-          headers: {
-            'content-type': 'application/json'
-          }
-        })
-          .then(response => response.json())
-          .then(data => {
-            alert(`Article ${this.article.title} Added`)
-            this.article.title = ''
-            this.article.body = ''
-            this.fetchArticles()
+      if (this.article.title == '') {
+        alert('Title and body must be filled')
+      } else {
+        if (this.edit === false) {
+          fetch('http://localhost:8000/articles-api/new/', {
+            method: 'post',
+            body: JSON.stringify(this.article),
+            headers: {
+              'content-type': 'application/json'
+            }
           })
-          .catch(error => console.log(error))
-      } else if (this.edit === true) {
-        fetch(`http://localhost:8000/articles-api/update/${this.article.id}/`, {
-          method: 'put',
-          body: JSON.stringify(this.article),
-          headers: {
-            'content-type': 'application/json'
-          }
-        })
-          .then(response => response.json())
-          .then(data => {
-            alert(`Article ${this.article.title} Updated`)
-            this.article.title = ''
-            this.article.body = ''
-            this.edit = false
-            this.update = ''
-            this.fetchArticles()
+            .then(response => response.json())
+            .then(data => {
+              alert(`Article ${this.article.title} Added`)
+              this.article.title = ''
+              this.article.body = ''
+              this.fetchArticles()
+            })
+            .catch(error => console.log(error))
+        } else if (this.edit === true) {
+          fetch(`http://localhost:8000/articles-api/update/${this.article.id}/`, {
+            method: 'put',
+            body: JSON.stringify(this.article),
+            headers: {
+              'content-type': 'application/json'
+            }
           })
-          .catch(error => console.log(error))
+            .then(response => response.json())
+            .then(data => {
+              alert(`Article ${this.article.title} Updated`)
+              this.article.title = ''
+              this.article.body = ''
+              this.edit = false
+              this.update = ''
+              this.fetchArticles()
+            })
+            .catch(error => console.log(error))
+        }
       }
     },
     editArticle (article) {
