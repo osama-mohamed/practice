b89c1d326bf04c1888373f044e4f04d3f6d29212
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-04-04 19:09:52 +0200

    fix pagination

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -10,7 +10,7 @@
         <textarea class="form-control" v-model="article.body" placeholder="Body"></textarea>
       </div>
       <button type="submit" class="btn btn-success btn-block" v-if="!update">Add</button>
-      <button type="submit" class="btn btn-success btn-block update" v-if="update">{{update}}</button>
+      <button type="submit" class="btn btn-success btn-block" v-if="update">{{update}}</button>
       <button type="button" v-if="update" @click="changeToAdd" class="btn btn-info btn-block mb-4">Click to change to add mode</button>
     </form>
     <h2 class="text-center mb-4">Articles</h2>
@@ -23,14 +23,39 @@
         <li v-bind:class="[{disabled: !pagination.previous_page_url}]" class="page-item">
           <a class="page-link" href="#" @click.prevent="fetchArticles(pagination.previous_page_url)">Previous</a>
         </li>
-        <li class="page-item disabled" v-if="pagination.current_page">
+       <!--<li class="page-item disabled" v-if="pagination.current_page">
           <a class="page-link text-dark" href="#">
             Page {{pagination.current_page.substring(pagination.current_page.indexOf('=') +1) -1}} of {{Math.round(pagination.last_page /pagePagination)}}</a>
-            <!--Page {{pagination.current_page.substring(44 + 1) -1}} of {{Math.round(pagination.last_page /5)}}</a>-->
+            &lt;!&ndash;Page {{pagination.current_page.substring(44 + 1) -1}} of {{Math.round(pagination.last_page /5)}}</a>&ndash;&gt;
         </li>
         <li class="page-item disabled" v-else>
           <a class="page-link text-dark" href="#">
-            Page {{Math.round(pagination.last_page /pagePagination)}} of {{Math.round(pagination.last_page /pagePagination)}}</a>
+            Page {{Math.round(pagination.last_page /pagePagination+1)}} of {{Math.round(pagination.last_page /pagePagination+1)}}</a>
+        </li>-->
+
+        <li class="page-item disabled" v-if="pagination.current_page && Number.isInteger(pagination.last_page/pagePagination)">
+          <a class="page-link text-dark" href="#">
+            Page {{pagination.current_page.substring(pagination.current_page.indexOf('=') +1) -1}}
+            of {{Math.round(pagination.last_page /pagePagination)}}
+          </a>
+        </li>
+        <li class="page-item disabled" v-else-if="!pagination.current_page && Number.isInteger(pagination.last_page/pagePagination)">
+          <a class="page-link text-dark" href="#">
+            Page {{Math.round(pagination.last_page /pagePagination)}}
+            of {{Math.round(pagination.last_page /pagePagination)}}</a>
+        </li>
+
+
+        <li class="page-item disabled" v-else-if="pagination.current_page && pagination.last_page">
+          <a class="page-link text-dark" href="#">
+            Page {{Math.round(pagination.current_page.substring(pagination.current_page.indexOf('=') +1) -1)}}
+            of {{Math.floor(pagination.last_page /pagePagination)+1}}
+          </a>
+        </li>
+        <li class="page-item disabled" v-else-if="!pagination.current_page && pagination.last_page">
+          <a class="page-link text-dark" href="#">
+            Page {{Math.floor(pagination.last_page /pagePagination)+1}}
+            of {{Math.floor(pagination.last_page /pagePagination)+1}}</a>
         </li>
 
         <li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item">
@@ -39,10 +64,10 @@
 
 
         <li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item" v-if="Number.isInteger(pagination.last_page/pagePagination)">
-          <a class="page-link" href="#" @click.prevent="fetchArticles(pagination.next_page_url.substring(0, pagination.next_page_url.indexOf('=')) + '=' + Math.round(pagination.last_page/pagePagination))">Last Page 1</a>
+          <a class="page-link" href="#" @click.prevent="fetchArticles(pagination.next_page_url.substring(0, pagination.next_page_url.indexOf('=')) + '=' + Math.round(pagination.last_page/pagePagination))">Last Page</a>
         </li>
         <li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item" v-else-if="pagination.last_page">
-          <a class="page-link" href="#" @click.prevent="fetchArticles(pagination.next_page_url.substring(0, pagination.next_page_url.indexOf('=')) + '=' + Math.round(pagination.last_page/pagePagination+1))">Last Page 2</a>
+          <a class="page-link" href="#" @click.prevent="fetchArticles(pagination.next_page_url.substring(0, pagination.next_page_url.indexOf('=')) + '=' + Math.floor(pagination.last_page/pagePagination+1))">Last Page</a>
         </li>
       </ul>
     </nav>
