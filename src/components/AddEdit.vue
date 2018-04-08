b32ea8d0748e195ae38d5528f0f148187672feb8
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-04-08 14:19:25 +0200

    check if fields has been filled

--- src/components/AddEdit.vue
+++ src/components/AddEdit.vue
@@ -6,17 +6,27 @@
     <br>
     <form @submit.prevent="addArticle" class="mb-4">
       <div class="form-group">
-        <input type="text" class="form-control" v-model="dropzoneOptions.params.title" placeholder="Title">
+        <input type="text" class="form-control" v-model="dropzoneOptions.params.title" placeholder="Title" ref="title">
       </div>
       <div class="form-group">
-        <textarea class="form-control" v-model="dropzoneOptions.params.body" placeholder="Body"></textarea>
+        <textarea class="form-control" v-model="dropzoneOptions.params.body" placeholder="Body" ref="body"></textarea>
       </div>
-      <vueDropzone
-        ref="myVueDropzoneref"
-        id="myVueDropzone"
-        :options="dropzoneOptions"
-        @vdropzone-queue-complete="resetDropzone"
-      ></vueDropzone>
+      <div v-if="!update">
+        <vueDropzone
+          ref="myVueDropzoneref"
+          id="myVueDropzone"
+          :options="dropzoneOptions"
+          @vdropzone-queue-complete="resetDropzone"
+        ></vueDropzone>
+      </div>
+      <div v-if="update">
+        <vueDropzone
+          ref="myVueDropzoneref2"
+          id="myVueDropzone"
+          :options="dropzoneOptions2"
+          @vdropzone-queue-complete="resetDropzone"
+        ></vueDropzone>
+      </div>
       <button type="submit" class="btn btn-success btn-block" v-if="!update">Add</button>
       <button type="submit" class="btn btn-success btn-block" v-if="update">{{update}}</button>
       <button type="button" v-if="update" @click="changeToAdd" class="btn btn-info btn-block mb-4">Click to change to add mode</button>
@@ -54,6 +64,19 @@ export default {
           body: ''
         },
       },
+      dropzoneOptions2: {
+        url: url,
+        dictDefaultMessage: "Update",
+        thumbnailWidth: 150,
+        maxFilesize: 0.5,
+        maxFiles: 1,
+        addRemoveLinks: true,
+        autoProcessQueue: false,
+        params: {
+          title: '',
+          body: ''
+        },
+      },
       article_id: '',
       edit: false,
       update: '',
@@ -70,24 +93,33 @@ export default {
       }, 3000)
     },
     addArticle () {
-      if (this.dropzoneOptions.params.title === '') {
-        alert('Title and body must be filled')
+      if (this.dropzoneOptions.params.title === '' ) {
+        alert('Title must be filled')
+        this.$refs.title.focus()
+      } else if (this.dropzoneOptions.params.body === '') {
+        alert('Body must be filled')
+        this.$refs.body.focus()
+      } else if (this.$refs.myVueDropzoneref.$el.dropzone.files[0] === undefined) {
+        alert('Picture must be filled')
+        document.getElementById('myVueDropzone').click()
       } else {
         if (this.edit === false) {
           this.$refs.myVueDropzoneref.processQueue()
         } else if (this.edit === true) {
           fetch(`http://localhost:8000/articles-api/update/${this.article_id}/`, {
             method: 'put',
-            body: JSON.stringify(this.dropzoneOptions.params),
+            body: JSON.stringify(this.dropzoneOptions2.params),
             headers: {
               'content-type': 'application/json'
             }
           })
             .then(response => response.json())
             .then(data => {
+              this.$refs.myVueDropzoneref2.processQueue()
               alert(`Article ${this.dropzoneOptions.params.title} Updated`)
               this.dropzoneOptions.params.title = ''
               this.dropzoneOptions.params.body = ''
+              this.$refs.myVueDropzoneref2.removeAllFiles(true)
               this.edit = false
               this.update = ''
               this.$parent.fetchArticles()
@@ -106,6 +138,7 @@ export default {
     changeToAdd () {
       this.dropzoneOptions.params.title = ''
       this.dropzoneOptions.params.body = ''
+      this.$refs.myVueDropzoneref.removeAllFiles(true)
       this.edit = false
       this.update = ''
       this.$parent.fetchArticles()
