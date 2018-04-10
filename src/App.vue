34a74293140ece764e7a2383e4c645a0e2781358
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-04-10 16:39:31 +0200

    setting up app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <v-app> <!-- dark -->
+      <v-toolbar>
+        <v-toolbar-title>Meetup</v-toolbar-title>
+        <v-toolbar-items>
+          <v-btn flat>view meet up</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <router-view/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/*@import '~vuetify/src/stylus/main'*/
+
+</style>
