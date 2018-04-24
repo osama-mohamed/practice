3fb2eb64e66811fccf4e3cc20b283fc7174b2bde
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-04-24 12:16:32 +0200

    start frontend

---
+++ pages/about/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <section id="about-page">
+    <h1>Hi</h1>
+    <p>This is about me page</p>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <section id="posts">
+    <PostPreview
+      v-for='post in posts'
+      :key='post.id'
+      :id='post.id'
+      :title='post.title'
+      :excerpt='post.previewText'
+      :thumbnailImage='post.thumbnailUrl'
+    ></PostPreview>
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Blog/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  },
+  data () {
+    return {
+      posts: [
+        {
+          id: 'first-post',
+          title: 'first post',
+          previewText: 'here some text',
+          thumbnailUrl: 'http://placehold.it/300/300'
+        },
+        {
+          id: 'second-post',
+          title: 'second post',
+          previewText: 'here some text',
+          thumbnailUrl: 'http://placehold.it/300/400'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#posts {
+  padding-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+@media (min-width: 55rem) {
+  #posts {
+    flex-direction: row;
+  }
+}
+</style>
