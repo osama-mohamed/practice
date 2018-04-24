7b364b3e88582fb259a77a23acba78c90b24bb3c
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-04-24 22:57:29 +0200

    build for production

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 const pkg = require('./package')
+const axios = require('axios')
 
 module.exports = {
   mode: 'universal',
+  // mode: 'spa',
 
   /*
   ** Headers of the page
@@ -42,8 +44,26 @@ module.exports = {
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
-    ['storyblok-nuxt', {accessToken: 'Abv6grUYmH2gCJdHN7rAgAtt', cacheProvider: 'memory'}],
+    ['storyblok-nuxt', {
+      accessToken: 
+        process.env.NODE_ENV == 'production' ? 'ocwrqGf1QPqIRpQVoKYjfAtt' : 'Abv6grUYmH2gCJdHN7rAgAtt',
+      cacheProvider: 'memory'
+    }],
   ],
+  generate: {
+    routes: function () {
+      return axios.get('https://api.storyblok.com/v1/cdn/stories?version=published&token=ocwrqGf1QPqIRpQVoKYjfAtt&starts_with=blog&cv=' + Math.floor(Date.now() / 1e3))
+        .then(res => {
+          const blogPosts = res.data.stories.map(blogPost => blogPost.full_slug)
+          return [
+            '/',
+            '/blog',
+            '/about',
+            ...blogPosts
+          ]
+        })
+    }
+  },
   /*
   ** Axios module configuration
   */
