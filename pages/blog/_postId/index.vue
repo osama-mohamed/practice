be125ad150166ec80a9cb728fdbd92cbb8573d86
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-04-24 21:25:18 +0200

    end storyblok

--- pages/blog/_postId/index.vue
+++ pages/blog/_postId/index.vue
@@ -14,7 +14,8 @@
 export default {
   asyncData (context) {
     return context.app.$storyapi.get('cdn/stories/blog/' + context.params.postId, {
-      version: 'draft'
+      // version: context.isDev ? 'draft' : 'published',
+      version: process.env.NODE_ENV == 'production' ? 'published' : 'draft'
     })
       .then(res => {
         return {
