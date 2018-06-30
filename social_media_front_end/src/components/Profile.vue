cc3c48ed1fdf848fb8499fdd8bdd4f12911f2149
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-06-30 18:23:12 +0200

    add comment form

--- social_media_front_end/src/components/Profile.vue
+++ social_media_front_end/src/components/Profile.vue
@@ -8,13 +8,12 @@
         <div class="row">
           <div v-for="post in posts" :key="post.id" class="card col-sm-6 offset-sm-3 my-2" style="width: 49%;">
             <div>
-              <img class="mr-2 mt-4" style="border-radius: 50%; max-width: 40px;" v-if="post.image" :src="'static/uploads/' + post.image" :alt="post.post">
-              <span style="top: 2px; position: relative; font-size: 18px; font-weight: bold; color: #365899;">{{user.username}}</span>
-              <!-- <span style="position: relative; top: 14px; left: -14px;">{{post.added | DateTime}}</span> -->
-              <span style="position: absolute; top: 44px; left: 69px; font-size: 12px;">{{post.added | DateTime}}</span>
+              <img class="mr-2 mt-4" style="border-radius: 50%; width: 40px; height: 40px;" v-if="post.image" :src="'static/uploads/' + post.image" :alt="post.post">
+              <span style="top: 3px; position: relative; font-size: 14px; font-weight: 600; color: #365899;">{{user.username}}</span>
+              <span style="position: absolute; top: 46px; left: 69px; font-size: 12px;">{{post.added | DateTime}}</span>
             </div>
-            <div class="dropdown show">
-              <a style="position: relative; left: 496px; top: -54px; cursor: pointer; font-weight: 900;" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <div class="">
+              <a style="position: relative; left: 496px; top: -50px; cursor: pointer; font-weight: 900;" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 ...
               </a>
               <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -22,18 +21,17 @@
                 <a class="dropdown-item" href="#">Delete</a>
               </div>
             </div>
-            <a :href="'static/uploads/' + post.image" target="_blank" class="mt-4">
-              <img class="card-img-top" v-if="post.image" :src="'static/uploads/' + post.image" :alt="post.post">
-            </a>
-            <div class="card-body">
-              <!-- <img class="card-img-top mr-2" style="border-radius: 50%; max-width: 40px;" v-if="post.image" :src="'static/uploads/' + post.image" :alt="post.post">
-              <span style="top: -8px; position: relative; font-size: 18px; font-weight: bold; color: #365899;">{{user.username}}</span> -->
-              <!-- <span style="position: relative; top: 14px; left: -14px;">{{post.added | DateTime}}</span> -->
-              <!-- <span style="position: absolute; bottom: 130px; left: 87px; font-size: 12px;">{{post.added | DateTime}}</span> -->
-              <p class="card-text">{{post.post}}</p>
-              <h5 class="card-title">#{{post.id}}</h5>
-              <h5 class="card-title">{{post.updated | DateTime}}</h5>
-              <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            <p class="card-text mt-1">{{post.post}}</p>
+            <div class="mb-3">
+              <a :href="'static/uploads/' + post.image" target="_blank" class="mt-4">
+                <img class="card-img-top" v-if="post.image" :src="'static/uploads/' + post.image" :alt="post.post">
+              </a>
+            </div>
+            <div class="mb-3">
+              <form class="form-group">
+                <img class="mr-2 mt-4" style="border-radius: 50%; width: 40px; height: 40px;" v-if="post.image" :src="'static/uploads/' + post.image" :alt="post.post">
+                <textarea type="text" class="form-control" placeholder="Write a comment ..." style="position: relative; top: -35px; left: 48px; max-width: 90%;"></textarea>
+              </form>
             </div>
           </div>
         </div>
