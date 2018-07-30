374383bcaad9d6ba7185f36580d3602a586b77d2
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-07-30 13:47:05 +0200

    form validation

--- products/forms.py
+++ products/forms.py
@@ -4,15 +4,53 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'product title',
+            }
+        )
+    )
+    email = forms.EmailField()
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                'class': 'new-class other-class',
+                'id': 'my-custom-id',
+                'placeholder': 'product description',
+                'rows': 12,
+                'cols': 25
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
     class Meta:
         model = Product
         fields = [
             'title',
             'description',
             'price',
-            'summary',
-            'featured',
+            # 'summary',
+            # 'featured',
         ]
+    def clean_title(self, *args, **keargs):
+        title = self.cleaned_data.get('title')
+        # if 'osama' in title:
+        #     return title
+        # else:
+        #     raise forms.ValidationError('This is not a valid title')
+        if not 'osama' in title:
+            raise forms.ValidationError('This is not a valid title')
+        return title
+    
+    def clean_email(self, *args, **keargs):
+        email = self.cleaned_data.get('email')
+        if not email.endswith('edu'):
+            raise forms.ValidationError('This is not a valid email')
+        return email
+        
+
 
 
 class RawProductForm(forms.Form):
