5264b847bb4356fb97ab8a2d916f542ba3361d0d
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-07-30 13:57:36 +0200

    initial values for forms

--- products/views.py
+++ products/views.py
@@ -30,15 +30,38 @@ from .forms import ProductForm, RawProductForm
 #     return render(request, 'products/product_create.html', context)
 
 
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'products/product_create.html', context)
+
+
+
+
+
+
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
+    initial_data = {
+        'title': 'This is initial title ',
+    }
+    obj = Product.objects.get(id=1)
+    form = ProductForm(request.POST or None, initial=initial_data, instance=obj)
     if form.is_valid():
         form.save()
-        form = ProductForm()
     context = {
         'form': form
     }
     return render(request, 'products/product_create.html', context)
+
+
+
+
+
 
 
 def product_detail_view(request):
