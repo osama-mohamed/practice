091a216b85b03a970d044a6677cb02899135c1ec
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-07-30 10:04:34 +0200

    create new product using model form

--- products/views.py
+++ products/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here. 
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request, 'products/product_create.html', context)
+
+
 def product_detail_view(request):
     qs = Product.objects.get(id=1)
     # context = {
