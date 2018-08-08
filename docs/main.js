ae61ce2fd783b011216f4dee414039bd752803dc
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-08-08 15:32:04 +0200

    build for production

---
+++ docs/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker({
+        disableWeekends: true,
+        yearRange: 2
+    });
+    $('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy();
+});
