1880cb7270e9f04025e1ca4b13bc954159bc394d
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-08-11 14:43:23 +0200

    add link to : add new, update

--- courses/models.py
+++ courses/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 
 class Course(models.Model):
     title = models.CharField(max_length=120)
+
+    def get_absolute_url(self):
+        return reverse('courses:courses_detail', kwargs={'id': self.id})
+
+    def get_absolute_update_url(self):
+        return reverse('courses:courses_update', kwargs={'id': self.id})
+
+    # def get_absolute_delete_url(self):
+    #     return reverse('courses:courses_delete', kwargs={'id': self.id})
