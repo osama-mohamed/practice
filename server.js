de00c4c478f8909c38870b00cb80a640aa7302ed
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-09-19 19:01:11 +0200

    start express app

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-var app = require("./app");
-var port = 8000;
-
-http.createServer(app.handleRequest).listen(port, () => {
-  console.log("Server started on port " + port);
-});
