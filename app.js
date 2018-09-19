6b75504769660aab8f1f0cc684eacb9b91e70144
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-09-19 09:23:31 +0200

    accessing modules

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var http = require("http");
+var module1 = require("./module1");
 var port = 8000;
 
 function onRequest(request, response) {
   response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write("Hello osama");
+  response.write(module1.myString);
+  module1.myFunction();
   response.end();
 }
 http.createServer(onRequest).listen(port, () => {
---
+++ module1.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  console.log("function called");
+}
+
+var myString = "String!!!";
+
+module.exports.myFunction = myFunction;
+module.exports.myString = myString;
