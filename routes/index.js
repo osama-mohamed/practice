eb4d3eea88c6081aa955484b8b43136a6d88d710
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-09-20 14:07:51 +0200

    create, read, update, delete from mongo db

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,107 @@
 var express = require("express");
 var router = express.Router();
+var mongo = require("mongodb").MongoClient;
+var objectId = require("mongodb").ObjectID;
+var assert = require("assert");
+
+var url = "mongodb://localhost:27017/";
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", {
-    title: "From Validation",
-    success: req.session.success,
-    errors: req.session.errors
-  });
-  req.session.errors = null;
-  req.session.success = null;
+  res.render("index");
 });
 
-/* check validity*/
+// retrive data
+router.get("/get-data", function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(
+    url,
+    function(err, db) {
+      var dbn = db.db("test");
+      assert.equal(null, err);
+      var cursor = dbn.collection("user-data").find();
+      cursor.forEach(
+        function(doc, err) {
+          assert.equal(null, err);
+          resultArray.push(doc);
+        },
+        function() {
+          db.close();
+          console.log("Done!");
+          res.render("index", { items: resultArray });
+        }
+      );
+    }
+  );
+});
 
-router.post("/submit", function(req, res, next) {
-  req.check("email", "Invalid email address").isEmail();
-  req
-    .check("password", "Password is invalid")
-    .isLength({ min: 8 })
-    .equals(req.body.confirmPassword);
+// insert data
+router.post("/insert", function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  mongo.connect(
+    url,
+    function(err, db) {
+      assert.equal(null, err);
+      var dbn = db.db("test");
+      dbn.collection("user-data").insertOne(item, function(err, result) {
+        assert.equal(null, err);
+        console.log("Item inserted");
+        db.close();
+      });
+    }
+  );
+  res.redirect("/");
+});
 
-  var errors = req.validationErrors();
-  if (errors) {
-    req.session.errors = errors;
-    req.session.success = false;
-  } else {
-    req.session.success = true;
-  }
+// update data
+router.post("/update", function(req, res, next) {
+  var id = req.body.id;
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  mongo.connect(
+    url,
+    function(err, db) {
+      assert.equal(null, err);
+      var dbn = db.db("test");
+      dbn
+        .collection("user-data")
+        .updateOne({ _id: objectId(id) }, { $set: item }, function(
+          err,
+          result
+        ) {
+          assert.equal(null, err);
+          console.log("Item updated");
+          db.close();
+        });
+    }
+  );
+  res.redirect("/");
+});
+
+// delete data
+router.post("/delete", function(req, res, next) {
+  var id = req.body.id;
+  mongo.connect(
+    url,
+    function(err, db) {
+      assert.equal(null, err);
+      var dbn = db.db("test");
+      dbn
+        .collection("user-data")
+        .deleteOne({ _id: objectId(id) }, function(err, result) {
+          assert.equal(null, err);
+          console.log("Item deleted");
+          db.close();
+        });
+    }
+  );
   res.redirect("/");
 });
 
