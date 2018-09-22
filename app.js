3ebb95e5b4de1d93956eba2bf9533c96cb2d5e38
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-09-22 22:54:30 +0200

    setup express, pug

--- app.js
+++ app.js
@@ -1,104 +1,49 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
-var expressValidator = require("express-validator");
-var mongojs = require("mongojs");
-var db = mongojs("test", ["users"]);
-var ObjectId = mongojs.ObjectId;
+const express = require("express");
+const path = require("path");
 
-var app = express();
+// init app
+const app = express();
 
-/* var logger = function(req, res, next) {
-  console.log("Logging ...");
-  next();
-};
-
-app.use(logger); */
-
-// view engine
-app.set("view engine", "ejs");
+// load view engine
 app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-// body parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// static path
-app.use(express.static(path.join(__dirname, "public")));
-
-// global vars
-app.use(function(req, res, next) {
-  res.locals.errors = null;
-  next();
-});
-
-// express-validator middleware options
-app.use(
-  expressValidator({
-    errorFormatter: function(param, msg, value) {
-      var namespace = param.split("."),
-        root = namespace.shift(),
-        formParam = root;
-
-      while (namespace.length) {
-        formParam += "[" + namespace.shift() + "]";
-      }
-      return {
-        param: formParam,
-        msg: msg,
-        value: value
-      };
-    }
-  })
-);
-
-app.get("/", function(req, res) {
-  db.users.find(function(err, docs) {
-    res.render("index", {
-      title: "Customers",
-      users: docs
-    });
+// home route
+app.get("/", (req, res) => {
+  let articles = [
+      {
+          id: 1,
+          title: 'Article one',
+          author: 'OSAMA MOHAMED',
+          body: 'This is article one'
+      },
+      {
+          id: 2,
+          title: 'Article two',
+          author: 'OSAMA',
+          body: 'This is article two'
+      },
+      {
+          id: 3,
+          title: 'Article three',
+          author: 'OS',
+          body: 'This is article three'
+      },
+  ];
+  res.render("index", {
+      title: 'Articles',
+      articles: articles
   });
 });
 
-app.post("/users/add", function(req, res) {
-  req.checkBody("first_name", "First name is required").notEmpty();
-  req.checkBody("last_name", "Last name is required").notEmpty();
-  req.checkBody("email", "Email is required").notEmpty();
-
-  var errors = req.validationErrors();
-
-  if (errors) {
-    res.render("index", {
-      title: "Customers",
-      users: users,
-      errors: errors
-    });
-  } else {
-    var newUser = {
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email
-    };
-    db.users.insert(newUser, function(err, result) {
-      if (err) {
-        console.log(err);
-      }
-      res.redirect("/");
-    });
-  }
-});
-
-app.delete("/users/delete/:id", function(req, res) {
-  console.log(req.params.id);
-  db.users.remove({ _id: ObjectId(req.params.id) }, function(err, result) {
-    if (err) {
-      console.log(err);
-    }
-    res.redirect("/");
+// add new article route
+app.get("/articles/add", (req, res) => {
+  res.render("add_article", {
+      title: 'Add Article'
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000 ...");
+// start server
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
 });
