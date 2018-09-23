d43cd11f3af6ba71d28096d28b4e54e76ee573f1
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-09-23 11:15:51 +0200

    update article with mongoose

--- app.js
+++ app.js
@@ -89,6 +89,38 @@ app.post("/articles/add", (req, res) => {
   });
 });
 
+// load edit article foarm
+app.get("/article/edit/:id", (req, res) => {
+  Article.findById(req.params.id, (err, article) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("edit_article", {
+        title: "Edit Article",
+        article: article
+      });
+    }
+  });
+});
+
+// save edited article
+app.post("/article/edit/:id", (req, res) => {
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id: req.params.id}
+  Article.update(query, article, err => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect("/article/" + req.params.id);
+    }
+  });
+});
+
 // start server
 app.listen(3000, () => {
   console.log("Server started on port 3000");
