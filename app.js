e9cf23026ea939baea8a8d3c28bf063a29213c72
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-09-29 21:36:49 +0200

    multer file upload

--- app.js
+++ app.js
@@ -1,125 +1,78 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const favicon = require("serve-favicon");
+const multer = require("multer");
+const ejs = require("ejs");
 const path = require("path");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const expressValidator = require("express-validator");
-const flash = require("connect-flash");
-const passport = require("passport");
-const config = require("./config/database");
-
-// connect to database
-mongoose.connect(
-  config.database,
-  { useNewUrlParser: true }
-);
-let db = mongoose.connection;
-
-// check DB connection
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
-// check for DB errors
-db.on("error", err => {
-  console.log(err);
-});
-
-// init app
+const port = process.env.PORT || 3000;
 const app = express();
 
-// bring in Article Model
-let Article = require("./models/article");
-
-// load view engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-// body parser middleware
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
-
-// set public folder
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-// set favicon
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
-// app.use('/favicon.ico', express.static('images/favicon.ico'));
-// app.use(favicon(__dirname + '/public/images/favicon.ico'));
-
-// express session middleware
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true
-    // cookie: { secure: true }
-  })
-);
-
-// express messages middleware
-app.use(require("connect-flash")());
-app.use(function(req, res, next) {
-  res.locals.messages = require("express-messages")(req, res);
-  next();
+// set storage engine
+const storage = multer.diskStorage({
+  // file destination & path on req.file
+  destination: path.join(__dirname, "public", "uploads"),
+  filename: function(req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
 });
 
-// Express Validator Middleware
-app.use(
-  expressValidator({
-    errorFormatter: function(param, msg, value) {
-      var namespace = param.split("."),
-        root = namespace.shift(),
-        formParam = root;
+// init upload
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 }, // size in bytes (1MB = 1000 * 1000 = 1000000)
+  fileFilter: function(req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).single("myImage");
 
-      while (namespace.length) {
-        formParam += "[" + namespace.shift() + "]";
-      }
-      return {
-        param: formParam,
-        msg: msg,
-        value: value
-      };
-    }
-  })
-);
+// check File Type
+function checkFileType(file, cb) {
+  const fileTypes = /jpeg|jpg|png|gif/; // allowed ext
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase()); // check ext
+  const mimeType = fileTypes.test(file.mimetype); // check mime
+  if (extName && mimeType) {
+    return cb(null, true);
+  } else {
+    return cb("Error: Images Only!");
+  }
+}
 
-// Passport Config
-require("./config/passport")(passport);
-// Passport Middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Set user in var if the user logged in
-app.get("*", (req, res, next) => {
-  res.locals.user = req.user || null;
-  next();
-});
-
-// home route
 app.get("/", (req, res) => {
-  Article.find({}, (err, articles) => {
+  res.render("index");
+});
+
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
     if (err) {
-      console.log(err);
+      if (err instanceof multer.MulterError) {
+        res.render("index", {
+          msg: "Error: File is too Large!"
+        });
+      } else {
+        res.render("index", {
+          msg: err
+        });
+      }
     } else {
-      res.render("index", {
-        title: "Articles",
-        articles: articles
-      });
+      if (req.file == undefined) {
+        res.render("index", {
+          msg: "Error: No File Selected!"
+        });
+      } else {
+        res.render("index", {
+          msg: "File Uploaded",
+          file: path.join("uploads", req.file.filename),
+          fileInfo: JSON.stringify(req.file)
+        });
+      }
     }
   });
 });
 
-// route files
-let articles = require("./routes/articles");
-let users = require("./routes/users");
-app.use("/articles", articles);
-app.use("/users", users);
-
-// start server
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
