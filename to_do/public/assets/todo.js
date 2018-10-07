614402cc49d157fd52c1298d8deac3ee55abfbba
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-07 17:57:01 +0200

    make requests with ajax

--- to_do/public/assets/todo.js
+++ to_do/public/assets/todo.js
@@ -7,13 +7,23 @@ $(document).ready(() => {
       url: "/",
       data: todo,
       success: data => {
-        location.reload();
+        $("ul").append(
+          '<li data-value="' +
+            data.item +
+            '" data-done="false">' +
+            ($("ul li").length + 1) +
+            " - " +
+            data.item +
+            '<button class="deleteItem">X</button></li><br>'
+        );
+        item.val("").blur();
+        fetchData();
       }
     });
     return false;
   });
 
-  $(".deleteItem").on("click", function(event) {
+  $("html").on("click", ".deleteItem", function(event) {
     event.stopPropagation();
     let item = $(this)
       .parent()
@@ -23,12 +33,15 @@ $(document).ready(() => {
       type: "DELETE",
       url: "/" + item,
       success: data => {
-        location.reload();
+        const item = $(this).parent();
+        item.next("br").remove();
+        item.fadeOut().remove();
+        fetchData();
       }
     });
   });
 
-  $("li").on("click", function() {
+  $("html").on("click", "li", function() {
     let item = $(this)
       .attr("data-value")
       .replace(/ /g, "-");
@@ -36,8 +49,49 @@ $(document).ready(() => {
       type: "POST",
       url: "/done/" + item,
       success: data => {
-        location.reload();
+        if (data.done === true) {
+          $(this).attr("data-done", false);
+        } else {
+          $(this).attr("data-done", true);
+        }
+        fetchData();
       }
     });
   });
 });
+
+function fetchData() {
+  $.ajax({
+    type: "GET",
+    url: "/all",
+    success: data => {
+      $("ul").empty();
+      let doneTodos = 0;
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].done === true) {
+          doneTodos += 1;
+          $("ul").append(
+            '<li data-value="' +
+              data[i].item +
+              '" data-done="true">' +
+              ($("ul li").length + 1) +
+              " - " +
+              data[i].item +
+              '<button class="deleteItem">X</button></li><br>'
+          );
+        } else {
+          $("ul").append(
+            '<li data-value="' +
+              data[i].item +
+              '" data-done="false">' +
+              ($("ul li").length + 1) +
+              " - " +
+              data[i].item +
+              '<button class="deleteItem">X</button></li><br>'
+          );
+        }
+      }
+      $("form span").text(`done : ${doneTodos} - ${data.length}`);
+    }
+  });
+}
