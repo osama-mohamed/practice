66cec60666b41607fcddb96e861878ea29c2790d
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-10 11:53:35 +0200

    create new post

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -2,6 +2,11 @@
   <div class="container">
     <h1>Latest Posts</h1>
     <!-- create new post -->
+    <div class="create-post">
+      <label for="create-post">Write post...</label>
+      <input type="text" id="create-post" v-model="text" placeholder="Create a new post">
+      <button @click="createPost">Post</button>
+    </div>
     <hr>
     <p class="error" v-if="error">{{ error }}</p>
     <div class="posts-container">
@@ -10,8 +15,9 @@
         :item="post"
         :index="index"
         :key="post._id">
-
+        <div class="created-at">
           {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()} ` }}
+        </div>
         <p class="text">{{ post.text }}</p>
       </div>
       
@@ -36,6 +42,12 @@ export default {
       this.posts = await PostService.getPosts();
     } catch (err) {
       this.error = err.message;
+    }
+  },
+  methods: {
+    async createPost() {
+      await PostService.insertPost(this.text);
+      this.posts = await PostService.getPosts();
     }
   }
 };
