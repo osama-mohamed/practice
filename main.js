62c193fb7a6f3b73df79f4ecdd4ba762492b1e33
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-11 11:25:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).on("ready", () => {
+  $.ajax({
+    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    success: data => {
+      const post = data.shift();
+      document.title = "Quote | " + post.title;
+      $("#quote-title").text(post.title);
+      $("#quote-content").html(post.content);
+      $("#quote-title-dash").html("&#8212;");
+      $("#quote-id").text("#" + post.ID);
+      $("#quote-link").attr("href", post.link);
+    },
+    cache: false
+  });
+});
