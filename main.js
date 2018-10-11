26d1070171d2d3181200d02716fe0c8ade45565e
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-11 12:08:54 +0200

    quotes api using ajax

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).on("ready", () => {
   $.ajax({
-    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    url:
+      "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
     success: data => {
       const post = data.shift();
       document.title = "Quote | " + post.title;
@@ -9,6 +10,7 @@ $(document).on("ready", () => {
       $("#quote-title-dash").html("&#8212;");
       $("#quote-id").text("#" + post.ID);
       $("#quote-link").attr("href", post.link);
+      $("#overlay").fadeOut();
     },
     cache: false
   });
