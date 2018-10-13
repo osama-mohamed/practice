04ff1769df49f7c31c749d3f199194e953cc0245
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-13 10:49:56 +0200

    simple auth system with jwt

--- app.js
+++ app.js
@@ -1,35 +1,57 @@
 const express = require("express");
-const path = require("path");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 3000;
 const app = express();
-const numberOfFiles = 4;
-const upload = require("./config/multerConfig")(
-  "users_profiles",
-  numberOfFiles
-);
-const multerError = require("./config/multerError");
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.render("index", { files: "" });
-});
-
-app.post("/upload", (req, res) => {
-  upload(req, res, err => {
-    const errorMsg = multerError(req, err, numberOfFiles);
-    const context = {
-      msg: errorMsg.msg,
-      files: ''
-    };
-    if (errorMsg.msg == "Files Uploaded") {
-      context.filesInfo = JSON.stringify(req.files);
-      context.files = req.files;
-    }
-    res.render("index", context);
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Home page"
   });
 });
+
+app.post("/api/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, "awesomeSecretKey", (err, auth) => {
+    if (err) {
+      res.json({
+        message: "Auth faild!"
+      });
+    } else {
+      res.json({
+        message: "post created successfully!",
+        auth
+      });
+    }
+  });
+});
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "osama mohamed",
+    email: "osama6osama6@gmail.com"
+  };
+  jwt.sign(
+    { user: user },
+    "awesomeSecretKey",
+    { expiresIn: "20m" },
+    (err, token) => {
+      res.json({
+        message: "Loged in successfully!",
+        token
+      });
+    }
+  );
+});
+
+function verifyToken(req, res, next) {
+  const header = req.headers.authorization;
+  if (typeof header !== "undefined") {
+    const newHeader = header.split(" ")[1];
+    req.token = newHeader;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
