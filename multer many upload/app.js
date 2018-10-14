9bff6fdd1de84412f744ad8990e9ac762b93d54e
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-14 14:44:39 +0200

    multer upload many

---
+++ multer many upload/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 3000;
+const app = express();
+const numberOfFiles = 4;
+const upload = require("./config/multerConfig")(
+  "users_profiles",
+  numberOfFiles
+);
+const multerError = require("./config/multerError");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", { files: "" });
+});
+
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    const errorMsg = multerError(req, err, numberOfFiles);
+    const context = {
+      msg: errorMsg.msg,
+      files: req.files,
+    };
+    if (errorMsg.msg == "Files Uploaded") {
+      context.filesInfo = JSON.stringify(req.files);
+    }
+    res.render("index", context);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+module.exports = {
+  numberOfFiles: numberOfFiles
+};
