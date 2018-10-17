c7ae5a7ba83276180d3ef290207f56a5edff21b1
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-17 18:53:59 +0200

    nexmo sms sender

---
+++ nexmo_sms_sender/public/js/main.js
@@ -0,0 +1,33 @@
+
+const numberInput = document.getElementById('number');
+const textInput = document.getElementById('msg');
+const button = document.getElementById('button');
+const response = document.querySelector('.response');
+
+button.addEventListener('click', send, false);
+
+const socket = io();
+socket.on('smsStatus', (data) => {
+    numberInput.value = '';
+    textInput.value = '';
+    response.innerHTML = `<h5>Text message sent to ${data.number} with id : ${data.id}</h5>`;
+});
+
+function send() {
+    const number = numberInput.value.replace(/\D/g, '');
+    const text = textInput.value;
+    
+    fetch('/', {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({number, text})
+    })
+    .then((res) => {
+        console.log('res', res);
+    })
+    .catch((err) => {
+        console.log('err', err);
+    });
+}
