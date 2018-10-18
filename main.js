a77f6a5410a54d380c681f8c9761ebb300904a54
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-18 14:41:00 +0200

    write type on button

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function getMovies(searchText) {
             <div class="well text-center">
             <img src="${movie.Poster}">
               <h5>${movie.Title}</h5>
-              <a onclick="movieSelected('${movie.imdbID}')" href="#" class="btn btn-primary">Movie Detail</a>
+              <a onclick="movieSelected('${movie.imdbID}')" href="#" class="btn btn-primary">${movie.Type} Detail</a>
             </div>
           </div>
         `;
@@ -81,7 +81,7 @@ function getMovie() {
           <h3>Plot</h3>
           ${movie.Plot}
           <hr>
-          <a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+          <a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View on IMDB</a>
           <a href="index.html" class="btn btn-default">Go Back To Search</a>
         </div>
       </div>
