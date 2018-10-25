93581cf1e0f1306798c18c99a76530fd8513bc2a
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-25 12:08:38 +0200

    setting up project files

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(
+    "mongodb://localhost:27017/videoidea",
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("MongoDB connected ...");
+  })
+  .catch(err => console.log(err));
+
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  const title = "Hello osama";
+  res.render("index", {
+    title: title
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
