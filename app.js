5f9d8ad1893035f52515565fa4a36a6e8b45e295
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-25 19:54:29 +0200

    split routes files

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const path = require("path");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+// LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,8 +23,7 @@ mongoose
   })
   .catch(err => console.log(err));
 
-require("./models/Idea");
-const Idea = mongoose.model("ideas");
+
 
 app.set("views", path.join(__dirname, "views"));
 app.engine(
@@ -32,9 +35,29 @@ app.engine(
 app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
+app.use(flash());
+app.use(
+  session({
+    secret: "secretKey",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+
+const IdeasRoutes = require('./routes/ideas');
+const UsersRoutes = require('./routes/users');
+app.use('/ideas', IdeasRoutes);
+app.use('/users', UsersRoutes);
 
 
 app.get("/", (req, res) => {
@@ -47,67 +70,6 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about");
 });
-
-app.get("/ideas", (req, res) => {
-  Idea.find({})
-    .sort({ date: "desc" })
-    .then(ideas => {
-      res.render("ideas/index", { ideas: ideas });
-    });
-});
-
-app.get("/ideas/add", (req, res) => {
-  res.render("ideas/add");
-});
-
-app.get("/ideas/edit/:id", (req, res) => {
-  Idea.findOne({_id: req.params.id}).then(idea => {
-    res.render("ideas/edit", {idea: idea});
-  });
-});
-
-app.post("/ideas", (req, res) => {
-  let errors = [];
-  if (!req.body.title) {
-    errors.push({ text: "Please add a title" });
-  }
-  if (!req.body.details) {
-    errors.push({ text: "Please add some details" });
-  }
-  if (errors.length > 0) {
-    res.render("ideas/add", {
-      errors: errors,
-      title: req.body.title,
-      details: req.body.details
-    });
-  } else {
-    const newIdea = {
-      title: req.body.title,
-      details: req.body.details
-    };
-    new Idea(newIdea).save().then(idea => {
-      res.redirect("/ideas");
-    });
-  }
-});
-
-app.put("/ideas/:id", (req, res) => {
-  Idea.findOne({_id: req.params.id}).then(idea => {
-    idea.title = req.body.title;
-    idea.details = req.body.details;
-    idea.save().then(idea => {
-      res.redirect('/ideas');
-    });
-  });
-});
-
-app.delete("/ideas/:id", (req, res) => {
-  Idea.deleteOne({_id: req.params.id}).then(() => {
-    res.redirect('/ideas');
-  });
-});
-
-
 
 
 app.listen(port, () => {
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get('/register', (req, res) => {
+  res.render('users/register');
+});
+
+router.get('/login', (req, res) => {
+  res.render('users/login');
+});
+
+module.exports = router;
