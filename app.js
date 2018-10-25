a3d4249b5a091404da8ea137f31d2c8257a8976e
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-25 22:06:15 +0200

    user login

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const flash = require("connect-flash");
 const session = require("express-session");
+const passport = require("passport");
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 require('./config/database');
+require("./config/passport")(passport);
 
 
 
@@ -34,12 +36,15 @@ app.use(
     saveUninitialized: true
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
   res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
   next();
 });
 
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const passport = require("passport");
-// LocalStrategy = require('passport-local').Strategy;
 
 router.get("/register", (req, res) => {
   res.render("users/register");
@@ -68,4 +67,14 @@ router.post("/register", (req, res) => {
     });
   }
 });
+
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/ideas',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
 module.exports = router;
