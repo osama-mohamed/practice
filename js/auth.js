a6d53a612c6450aaf6eeb1323310ee6c9359d403
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-04-22 20:31:54 +0200

    listen for auth status changes

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,13 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+  if (user) {
+    console.log('user logged in: ', user);
+  } else {
+    console.log('user logged out');
+  }
+});
+
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
