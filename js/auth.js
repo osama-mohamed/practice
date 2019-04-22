d1306072644db520eb01c6936e305fe8163f821d
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-04-22 20:07:57 +0200

    sign up user

--- js/auth.js
+++ js/auth.js
@@ -7,4 +7,12 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
+  // sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
 });
