3b3746039306402d8d373205e40e170a36a3dc04
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-04-22 19:49:03 +0200

    set up firebase

---
+++ js/auth.js
@@ -0,0 +1,10 @@
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+});
