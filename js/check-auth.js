58b717afc0fd5cf68171b494a22907f90f532d09
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-05-17 21:34:55 +0200

    make an admin, secure functions

--- js/check-auth.js
+++ js/check-auth.js
@@ -2,10 +2,14 @@
 auth.onAuthStateChanged(user => {
   if (user) {
     console.log('user logged in: ', user);
+    user.getIdTokenResult().then(idTokenResult => {
+      console.log(idTokenResult.claims);
+      user.admin = idTokenResult.claims.admin;
+      setupUI(user);
+    });
     // get data from db
     db.collection('guides').onSnapshot(snapshot => {
       setupGuides(snapshot.docs);
-      setupUI(user);
     }, err => console.log(err.message));
   } else {
     console.log('user logged out');
@@ -19,8 +23,12 @@ const setupUI = (user) => {
   const loggedOutLinks = document.querySelectorAll('.logged-out');
   const loggedInLinks = document.querySelectorAll('.logged-in');
   const accountDetails = document.querySelector('.account-details');
+  const adminItems = document.querySelectorAll('.admin');
   // toggle user elements
   if (user) {
+    if (user.admin) {
+      adminItems.forEach(item => item.style.display = 'block');
+    }
     loggedInLinks.forEach(item => item.style.display = 'block');
     loggedOutLinks.forEach(item => item.style.display = 'none');
     // account info
@@ -28,9 +36,11 @@ const setupUI = (user) => {
       accountDetails.innerHTML = `
         <div>Logged in as ${user.email}</div>
         <div>${doc.data().bio}</div>
+        <div class="pink-text">${user.admin ? 'Admin' : ''}</div>
       `;
     });
   } else {
+    adminItems.forEach(item => item.style.display = 'none');
     loggedInLinks.forEach(item => item.style.display = 'none');
     loggedOutLinks.forEach(item => item.style.display = 'block');
     // clear account info
