1a64392e4bbedb539fc72b030c4bd776031440f1
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-11-23 13:38:42 +0200

    read database data

--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 
-import os
+from articles.models import Article
 
 def home_view(request):
+  article_obj = Article.objects.get(id=1)
   HTML_STRING = f"""
-  <h1>Hi {os.getlogin()}!</h1>
+  <h1>#{article_obj.id}: {article_obj.title} !</h1>
+  <p>Hi {article_obj.content}!</p>
   """
   return HttpResponse(HTML_STRING)
