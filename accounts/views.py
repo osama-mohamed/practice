aa881768b639f6fa5b1c87800452b4f433123182
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-11-24 14:35:11 +0200

    user login model form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -16,15 +16,17 @@ def register_view(request):
 
 def login_view(request):
   if request.method == 'POST':
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    user = authenticate(request, username=username, password=password)
-    if user is None:
-      context = {'error': 'Invalid username or password.'}
-      return render(request, 'accounts/login.html', context)
-    login(request, user)
-    return redirect('/')
-  return render(request, 'accounts/login.html', {})
+    form = AuthenticationForm(request, data=request.POST)
+    if form.is_valid():
+      user = form.get_user()
+      login(request, user)
+      return redirect('/')
+  else:
+    form = AuthenticationForm(request)
+  context = {
+    'form': form
+  }
+  return render(request, 'accounts/login.html', context)
 
 
 def logout_view(request):
