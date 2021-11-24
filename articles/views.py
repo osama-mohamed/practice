efd88e13b4cbdda5bc1ab3a58f4eaa1d15dd755c
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-11-24 10:25:58 +0200

    basic id search

--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,21 @@ from django.shortcuts import render
 from .models import Article
 
 # Create your views here.
+def article_search_view(request):
+  query_dict = request.GET
+  try:
+    query = int(query_dict.get('q'))
+  except:
+    query = None
+  article_obj = None
+  if query is not None:
+    article_obj = Article.objects.get(id=query)
+  context = {
+    'object': article_obj,
+  }
+  return render(request, 'articles/search.html', context=context)
+
+
 def article_detail_view(request, id=None):
   article_obj = None
   if id is not None:
