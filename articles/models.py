74f10ebb5c5e82d632f965da6c2fd951eac161ea
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-11-26 15:09:02 +0200

    articles title slugify signals

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 from django.utils.text import slugify
 
 # Create your models here.
@@ -10,8 +11,14 @@ class Article(models.Model):
   updated = models.DateTimeField(auto_now=True)
   publish = models.DateField(auto_now_add=False, auto_now=False, null=True, blank=True)
 
-  def save(self, *args, **kwargs):
-    if self.slug is None:
-      self.slug = slugify(self.title)
-    super().save(*args, **kwargs)
+def article_pre_save(sender, instance, *args, **kwargs):
+    if instance.slug is None:
+      instance.slug = slugify(instance.title)
+pre_save.connect(article_pre_save, sender=Article)
 
+
+def article_post_save(sender, instance, created, *args, **kwargs):
+  if created:
+    instance.slug = slugify(instance.title)
+    instance.save()
+post_save.connect(article_post_save, sender=Article)
