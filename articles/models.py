501dc722d303c7e2e6dbe56d3d617c3cedc36001
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-11-27 11:21:53 +0200

    articles title slugify recursion

--- articles/models.py
+++ articles/models.py
@@ -1,25 +1,16 @@
 from django.db import models
 from django.db.models.signals import pre_save, post_save
-from django.utils.text import slugify
+from .utils import slugify_instance_title
 
 # Create your models here.
 class Article(models.Model):
   title = models.CharField(max_length=220)
-  slug = models.SlugField(blank=True, null=True)
+  slug = models.SlugField(unique=True, blank=True, null=True)
   content = models.TextField()
   timestamp = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
   publish = models.DateField(auto_now_add=False, auto_now=False, null=True, blank=True)
 
-def slugify_instance_title(instance, save=False):
-  slug = slugify(instance.title)
-  qs = Article.objects.filter(slug=slug).exclude(id=instance.id)
-  if qs.exists():
-    slug = f'{slug}-{qs.count() + 1}'
-  instance.slug = slug
-  if save:
-    instance.save()
-  return instance
 
 def article_pre_save(sender, instance, *args, **kwargs):
   if instance.slug is None:
