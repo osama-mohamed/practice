91f7dba4408607e7bf3f92d8cf7eb6ffbef463a8
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-11-27 16:35:24 +0200

    connect articles with user foreign key

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
@@ -5,6 +6,9 @@ from django.urls import reverse
 from .utils import slugify_instance_title
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
 class ArticleQuerySet(models.QuerySet):
   def search(self, query=None):
     if query is None or query == '':
@@ -30,6 +34,7 @@ class ArticleManager(models.Manager):
 
 
 class Article(models.Model):
+  user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
   title = models.CharField(max_length=220)
   slug = models.SlugField(unique=True, blank=True, null=True)
   content = models.TextField()
