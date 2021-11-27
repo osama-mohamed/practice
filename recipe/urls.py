affb48fbedf0be309fc1ff486b6fd71e191ae7fa
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-11-27 12:42:25 +0200

    articles slug dynamic urls

--- recipe/urls.py
+++ recipe/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('', home_view),
     path('articles/', article_search_view),
     path('articles/create/', article_create_view),
-    path('articles/<int:id>/', article_detail_view),
+    path('articles/<slug:slug>/', article_detail_view),
     path('login/', login_view),
     path('logout/', logout_view),
     path('register/', register_view),
