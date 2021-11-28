1ab9c2fbba44bf4435e64abd3be9194cddebfbc7
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-11-28 18:23:24 +0200

    recipes app models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
-
 # Create your models here.
+
+class Recipe(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  name = models.CharField(max_length=220)
+  description = models.TextField(blank=True, null=True)
+  directions = models.TextField(blank=True, null=True)
+  active = models.BooleanField(default=True)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+
+
+class RecipeIngredient(models.Model):
+  recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+  name = models.CharField(max_length=220)
+  quantity = models.CharField(max_length=50)
+  unit = models.CharField(max_length=50)
+  description = models.TextField(blank=True, null=True)
+  directions = models.TextField(blank=True, null=True)
+  active = models.BooleanField(default=True)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
