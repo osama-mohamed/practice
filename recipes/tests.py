0d8bd3fb9e29a90c83403e24e39cfd42f5f3c95b
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-11-30 10:08:47 +0200

    recipes unit validation test

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from .models import Recipe, RecipeIngredient
+
 # Create your tests here.
 
 User = get_user_model()
@@ -61,3 +63,36 @@ class RecipeTestCase(TestCase):
     ids = user.recipe_set.all().values_list("id", flat=True)
     qs = RecipeIngredient.objects.filter(recipe__id__in=ids)
     self.assertEqual(qs.count(), self.recipe_ingredient_count)
+
+  def test_unit_measure_validation(self):
+    valid_unit = 'ounce'
+    ingredient = RecipeIngredient(
+      name = 'New',
+      quantity = 10,
+      recipe = self.recipe_a,
+      unit = valid_unit
+    )
+    ingredient.full_clean()
+    
+  def test_unit_measure_validation_error(self):
+    invalid_unit = 'wrongunit'
+    with self.assertRaises(ValidationError):
+      ingredient = RecipeIngredient(
+        name = 'New',
+        quantity = 10,
+        recipe = self.recipe_a,
+        unit = invalid_unit
+      )
+      ingredient.full_clean()
+
+  def test_unit_measure_validation_error_list(self):
+    invalid_units = ['wrongunit', 'abc']
+    with self.assertRaises(ValidationError):
+      for unit in invalid_units:
+        ingredient = RecipeIngredient(
+          name = 'New',
+          quantity = 10,
+          recipe = self.recipe_a,
+          unit = unit
+        )
+        ingredient.full_clean()
