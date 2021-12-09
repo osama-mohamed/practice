52ccf69436ded39302021d117b61f581832b1c92
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-12-09 15:42:14 +0200

    meals app models update

--- meals/models.py
+++ meals/models.py
@@ -23,7 +23,7 @@ class MealStatus(models.TextChoices):
 
 
 class MealQuerySet(models.QuerySet):
-  def by_user_id(self, user_id):
+  def by_user_id(self, user_id): # Meal.objects.all().by_user_id() # if not in MealManager
     return self.filter(user_id=user_id)
 
   def by_user(self, user):
@@ -42,11 +42,38 @@ class MealQuerySet(models.QuerySet):
   def aborted(self):
     return self.filter(status=MealStatus.ABORTED)
 
+  def in_queue(self, recipe_id):
+    return self.pending().filter(recipe_id=recipe_id).exists()
+
 
 
 class MealManager(models.Manager):
   def get_queryset(self):
     return MealQuerySet(self.model, using=self._db)
+
+  def by_user_id(self, user_id): # Meal.objects.by_user_id()
+    return self.get_queryset().by_user_id(user_id)
+
+  def by_user(self, user):
+    return self.get_queryset().by_user(user)
+
+  def toggle_in_queue(self, user_id, recipe_id):
+    qs = self.get_queryset().all().by_user_id(user_id)
+    already_queued = qs.in_queue(recipe_id)
+    added = None
+    if already_queued:
+      recipe_qs = qs.filter(recipe_id=recipe_id)
+      recipe_qs.update(status=MealStatus.ABORTED)
+      added = False
+    else:
+      obj = self.model(
+        user_id=user_id,
+        recipe_id=recipe_id,
+        status=MealStatus.PENDING
+      )
+      obj.save()
+      added = True
+    return added
 
 
 class Meal(models.Model):
