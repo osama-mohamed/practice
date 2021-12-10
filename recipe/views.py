9d9fcf6d07648a62976d4a0f49a253d810e39923
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-12-10 18:23:36 +0200

    articles random ids from db another way

--- recipe/views.py
+++ recipe/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
 from articles.models import Article
 
-from random import paretovariate, randint
+from random import randint
 
 def home_view(request):
   article_queryset = Article.objects.all()
-  first_id = article_queryset.first().id
-  last_id = article_queryset.last().id
-  range_numbers = list(range(first_id, last_id+1))
+
+  # first_id = article_queryset.first().id
+  # last_id = article_queryset.last().id
+  # range_numbers = list(range(first_id, last_id+1))
+  # ids = list(article_queryset.values_list('id', flat=True).order_by('id'))
+  # list_deff_numbers = list(set(range_numbers) - set(ids)) 
+  # random_id = randint(first_id, last_id)
+  # if random_id in list_deff_numbers:
+  #   random_id = randint(first_id, last_id)
+
   ids = list(article_queryset.values_list('id', flat=True).order_by('id'))
-  list_deff_numbers = list(set(range_numbers) - set(ids)) 
-  random_id = randint(first_id, last_id)
-  if random_id in list_deff_numbers:
-    random_id = randint(first_id, last_id)
+  random_id = randint(ids[0], ids[-1])
+  if random_id not in ids:
+    random_id = randint(ids[0], ids[-1])
+
   article_obj = Article.objects.get(id=random_id)
   context = {
     'object': article_obj,
