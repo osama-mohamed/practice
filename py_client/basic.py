f004226a1564960d00cf32a14698c62326112679
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2022-02-28 13:31:44 +0200

    start api app

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,7 @@
 import requests
 
-endpoint = 'https://httpbin.org/anything'
+# endpoint = 'https://httpbin.org/anything'
+endpoint = 'http://localhost:8000/'
 res = requests.get(endpoint, json={'query': 'Hello OSAMA'})
-print(res.json())
+# print(res.json())
 print(res.status_code)
