46f4b09a6b56fc2ed53eac8e17507ca07f6bcebe
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2022-03-09 09:35:27 +0200

    model to dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,12 @@
 from django.http import JsonResponse
 from products.models import Product
+from django.forms.models import model_to_dict
 import json
 
 
 def api_home(request, *args, **kwargs):
-  # body = request.body
-  # data = {}
-  # try:
-  #   data = json.loads(body)
-  # except:
-  #   pass
-  # data['params'] = dict(request.GET)
-  # data['headers'] = dict(request.headers)
-  # data['content_type'] = request.content_type
   model_data = Product.objects.all().order_by('?').first()
   data = {}
   if model_data:
-    data['id'] = model_data.id
-    data['title'] = model_data.title
-    data['content'] = model_data.content
-    data['price'] = model_data.price
+    data = model_to_dict(model_data, fields=['id', 'title', 'price'])
   return JsonResponse(data)
