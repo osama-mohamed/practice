e2c4e25c70e2d48147441a630d79ba37116b07f6
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2022-03-09 10:00:45 +0200

    api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,16 @@
 from django.http import JsonResponse
 from products.models import Product
 from django.forms.models import model_to_dict
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 import json
 
 
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
   model_data = Product.objects.all().order_by('?').first()
   data = {}
   if model_data:
     data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-  return JsonResponse(data)
+  return Response(data)
--- backend/home/settings.py
+++ backend/home/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'products.apps.ProductsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
