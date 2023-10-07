2d6309db6964096200aaa42aaaf5661ecc2fb056
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-07 22:29:03 +0300

    list view

--- blog/urls.py
+++ blog/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from .views import redirect_somewhere
+from .views import post_model_list_view
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('redirect/', redirect_somewhere, name='redirect'),
-    path('blog/', include('blog.urls'))
+    path('', post_model_list_view, name='list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import PostModel
 # Create your views here.
+
+def post_model_list_view(request):
+  qs = PostModel.objects.all()
+  print(qs)
+  context = {
+    'object_list': qs
+  }
+  return render(request, "blog/list-view.html", context)
