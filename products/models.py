c1549099a9be73ad020cc83a4dd7df4b93cc4594
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-10 10:56:45 +0300

    choices update 2

--- products/models.py
+++ products/models.py
@@ -4,22 +4,18 @@ from django.db import models
 from .validators import validate_blocked_words
 
 
-
-
+  
 class Product(models.Model):
-  PUBLISH = 'PU'
-  DRAFT = 'DR '
-  PRIVATE = 'PR'
-  PIBLISH_STATE_CHOICES = (
-    # ('DB value', 'user display value')
-    (PUBLISH, 'Published'),
-    (DRAFT, 'Draft'),
-    (PRIVATE, 'Private'),
-  )
+  class ProductStateOptions(models.TextChoices):
+    PUBLISH = 'PU', 'Published'
+    DRAFT = 'DR', 'Draft'
+    PRIVATE = 'PR', 'Private'
+    # obj.get_state_display()
+    # state=Product.productstateoptions.PUBLISH
 
   title = models.CharField(max_length=120, validators=[validate_blocked_words])
-  state = models.CharField(max_length=2, default=DRAFT,
-                           choices=PIBLISH_STATE_CHOICES)
+  state = models.CharField(max_length=2, default=ProductStateOptions.DRAFT,
+                           choices=ProductStateOptions.choices)
   description = models.TextField(null=True)
   price = models.DecimalField(max_digits=10, decimal_places=2)
 
@@ -32,4 +28,4 @@ class Product(models.Model):
 
   @property
   def is_published(self):
-    return self.state == self.PUBLISH
+    return self.state == self.ProductStateOptions.PUBLISH
