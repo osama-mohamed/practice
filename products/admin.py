28788e8064de6c047942a3a2baef4149cf02f043
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-10 13:00:25 +0300

    datetime field

--- products/admin.py
+++ products/admin.py
@@ -4,4 +4,14 @@ from .models import Product
 # Register your models here.
 
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+  list_display = [field.name for field in Product._meta.get_fields()]
+  fields = ['title', 'state', 'description', 'price', 'timestamp', 'updated']
+  readonly_fields = ['timestamp', 'updated']
+  list_display_links = [field.name for field in Product._meta.get_fields() if field.name in ['id', 'title']]
+  list_editable = ['state', 'price']
+  class Meta:
+    model = Product
+
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 
 from .validators import validate_blocked_words
 
@@ -18,14 +18,28 @@ class Product(models.Model):
                            choices=ProductStateOptions.choices)
   description = models.TextField(null=True)
   price = models.DecimalField(max_digits=10, decimal_places=2)
+  publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
+  timestamp = models.DateTimeField(auto_now_add=True) # default=timezone.now
+  updated = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return self.title
 
   def save(self, *args, **kwargs):
     validate_blocked_words(self.title)
+    if self.state_is_published and self.publish_timestamp is None:
+      self.publish_timestamp = timezone.now()
+    else:
+      self.publish_timestamp = None
     super().save(*args, **kwargs)
 
   @property
-  def is_published(self):
+  def state_is_published(self):
     return self.state == self.ProductStateOptions.PUBLISH
+  
+  @property
+  def is_published(self):
+    publish_timestamp = self.publish_timestamp
+    return self.state_is_published and publish_timestamp < timezone.now()
+  # def is_published(self):
+  #   return self.state == self.ProductStateOptions.PUBLISH
