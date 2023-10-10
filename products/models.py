629b0f0b6003f1d5c7f3a0b0f137b6c5b2171f08
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-10 10:40:25 +0300

    choices

--- products/models.py
+++ products/models.py
@@ -3,10 +3,18 @@ from django.db import models
 
 from .validators import validate_blocked_words
 
-# Create your models here.
+
+PIBLISH_STATE_CHOICES = (
+  # ('DB value', 'user display value')
+  ('pu', 'Published'),
+  ('dr', 'Draft'),
+  ('pr', 'Private'),
+)
 
 class Product(models.Model):
   title = models.CharField(max_length=120, validators=[validate_blocked_words])
+  state = models.CharField(max_length=2, default='dr',
+                           choices=PIBLISH_STATE_CHOICES)
   description = models.TextField(null=True)
   price = models.DecimalField(max_digits=10, decimal_places=2)
 
@@ -16,3 +24,7 @@ class Product(models.Model):
   def save(self, *args, **kwargs):
     validate_blocked_words(self.title)
     super().save(*args, **kwargs)
+
+  @property
+  def is_published(self):
+    return self.state == 'pu'
