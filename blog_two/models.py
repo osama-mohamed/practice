0f5f6b30b2518b5a674a0e38b766ab1cfb65d055
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-14 11:52:16 +0300

    set up model

--- blog_two/models.py
+++ blog_two/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+PUBLISH_CHOICES = [
+  ('DR', 'Draft'),
+  ('PU', 'Publish'),
+  ('PR', 'Private'),
+]
+
+
+class Post(models.Model):
+  active = models.BooleanField(default=True)
+  title = models.CharField(max_length=240, verbose_name='Post Title')
+  content = models.TextField(null=True, blank=True)
+  publish = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
+
+  class Mete:
+    verbose_name = 'Post'
+    verbose_name_plural = 'Posts'
+
+  def __str__(self):
+    return self.title
