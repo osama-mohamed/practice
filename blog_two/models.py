9181b60217bb9a33b571e4b9a8eb2a324d4625a0
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-15 20:26:21 +0300

    custom field validation

--- blog_two/models.py
+++ blog_two/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+from django.utils import timezone
+from django.utils.encoding import smart_str
+
+from .validators import validate_author_email
+  
 
 PUBLISH_CHOICES = [
   ('DR', 'Draft'),
@@ -10,13 +15,17 @@ PUBLISH_CHOICES = [
 
 class Post(models.Model):
   active = models.BooleanField(default=True)
-  title = models.CharField(max_length=240, verbose_name='Post Title')
+  title = models.CharField(max_length=240, verbose_name='Post Title', unique=True)
   content = models.TextField(null=True, blank=True)
-  publish = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
+  publish = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')# docs.djangoproject.com/en/4.2/ref/models/fields/#choices
+  view_count = models.IntegerField(default=0)
+  publish_date = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
+  author_email = models.EmailField(max_length=240, validators=[validate_author_email,], null=True, blank=True)
 
   class Mete:
     verbose_name = 'Post'
     verbose_name_plural = 'Posts'
 
   def __str__(self):
-    return self.title
+    return smart_str(self.title) # docs.djangoproject.com/en/4.2/ref/unicode/#useful-utility-functions
+  
---
+++ blog_two/validators.py
@@ -0,0 +1,7 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_author_email(value):
+  if not '@' in value:
+    raise ValidationError('Not a vlid email')
+  return value
