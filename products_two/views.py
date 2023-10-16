50c4ea7d46f8e4c55d6be282ad73afdb78a8dfd5
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-16 21:53:00 +0300

    fbv vs cbv

--- products_two/views.py
+++ products_two/views.py
@@ -1,3 +1,23 @@
+from django.views.generic import View, ListView
+from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
 
-# Create your views here.
+
+@require_http_methods(['GET']) # only allow GET requests
+def product_list_view(request):
+  return render(request, 'temp', {})
+
+
+class ProductHomeView(View):
+  def get(self, request, *args, **kwargs):
+    return render(request, 'temp', {})
+  
+  def post(self, request, *args, **kwargs):
+    return render(request, 'temp', {})
+
+class ProductListView(ListView):
+  # model = Product
+  queryset = Product.objects.all()
+  template_name = 'temp'
+
+ProductListView.as_view()
