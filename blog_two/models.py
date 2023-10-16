74acdbf7d8ca88c9224d1c2b2588e65d8dc035cf
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-16 17:04:12 +0300

    override save method

--- blog_two/models.py
+++ blog_two/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from django.utils import timezone
 from django.utils.encoding import smart_str
+from django.utils.text import slugify
+
 
 from .validators import validate_author_email
   
@@ -16,6 +18,7 @@ PUBLISH_CHOICES = [
 class Post(models.Model):
   active = models.BooleanField(default=True)
   title = models.CharField(max_length=240, verbose_name='Post Title', unique=True)
+  slug = models.SlugField(null=True, blank=True)
   content = models.TextField(null=True, blank=True)
   publish = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')# docs.djangoproject.com/en/4.2/ref/models/fields/#choices
   view_count = models.IntegerField(default=0)
@@ -29,3 +32,8 @@ class Post(models.Model):
   def __str__(self):
     return smart_str(self.title) # docs.djangoproject.com/en/4.2/ref/unicode/#useful-utility-functions
   
+  def save(self, *args, **kwargs):
+    if not self.slug:
+      print(self.slug)
+      self.slug = slugify(self.title, allow_unicode=True) # allow_unicode=True for non-english languages => docs.djangoproject.com/en/4.2/ref/utils/#django.utils.text.slugify
+    super(Post, self).save(*args, **kwargs)
