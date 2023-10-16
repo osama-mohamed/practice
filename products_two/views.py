07d0098a408a7bbf2ce04dd625b21bea1f99a324
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-16 22:54:19 +0300

    get context data

--- products_two/views.py
+++ products_two/views.py
@@ -8,6 +8,10 @@ class ProductListView(ListView):
   model = Product
   template_name = 'products/product_list.html'
 
+  def get_context_data(self, *args, **kwargs):
+    context = super().get_context_data(*args, **kwargs)
+    return context
+
 
 class ProductDetailView(DetailView):
   # queryset = Product.objects.all()
