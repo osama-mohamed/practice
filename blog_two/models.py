34894a86712bd6bd54c370f86c2b4a6bb0acdf45
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-16 17:31:37 +0300

    pre & post save signals

--- blog_two/models.py
+++ blog_two/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 
 from django.utils import timezone
 from django.utils.encoding import smart_str
@@ -6,7 +7,7 @@ from django.utils.text import slugify
 
 
 from .validators import validate_author_email
-  
+
 
 PUBLISH_CHOICES = [
   ('DR', 'Draft'),
@@ -33,7 +34,22 @@ class Post(models.Model):
     return smart_str(self.title) # docs.djangoproject.com/en/4.2/ref/unicode/#useful-utility-functions
   
   def save(self, *args, **kwargs):
-    if not self.slug:
-      print(self.slug)
-      self.slug = slugify(self.title, allow_unicode=True) # allow_unicode=True for non-english languages => docs.djangoproject.com/en/4.2/ref/utils/#django.utils.text.slugify
+    # if not self.slug:
+    #   self.slug = slugify(self.title, allow_unicode=True) # allow_unicode=True for non-english languages => docs.djangoproject.com/en/4.2/ref/utils/#django.utils.text.slugify
     super(Post, self).save(*args, **kwargs)
+
+
+def post_pre_save_receiver(sender, instance, *args, **kwargs): # docs.djangoproject.com/en/4.2/ref/signals/#pre-save
+  if not instance.slug and instance.title:
+    instance.slug = slugify(instance.title, allow_unicode=True)
+  
+
+def post_post_save_receiver(sender, instance, created, *args, **kwargs): # docs.djangoproject.com/en/4.2/ref/signals/#post-save
+  if created:
+    if not instance.slug and instance.title:
+      instance.slug = slugify(instance.title, allow_unicode=True)
+      instance.save()
+
+
+pre_save.connect(post_pre_save_receiver, sender=Post)
+post_save.connect(post_post_save_receiver, sender=Post)
