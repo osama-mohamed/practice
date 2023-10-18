9917131fb0610588b700ca6692d7388eee3bae70
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-18 19:14:05 +0300

    cbv mixin

--- products_two/views.py
+++ products_two/views.py
@@ -5,24 +5,23 @@ from .models import Product, DigitalProduct
 
 
 
-class DigitalProductListView(ListView):
-  model = DigitalProduct
+class ProductTemplateMixin(object):
+  title = None
   template_name = 'products/product_list.html'
   
   def get_context_data(self, *args, **kwargs):
     context = super().get_context_data(*args, **kwargs)
-    context['title'] = 'Digital Downloads'
-    return context # digitalproduct_list
+    context['title'] = self.title
+    return context
 
-class ProductListView(ListView):
-  # queryset = Product.objects.all()
+class DigitalProductListView(ProductTemplateMixin, ListView):
+  model = DigitalProduct
+  title = 'Digital Products'
+
+
+class ProductListView(ProductTemplateMixin, ListView):
   model = Product
-  template_name = 'products/product_list.html'
-
-  def get_context_data(self, *args, **kwargs):
-    context = super().get_context_data(*args, **kwargs)
-    context['title'] = 'Products'
-    return context # product_list
+  title = 'Products'
 
 
 class ProductDetailView(DetailView):
