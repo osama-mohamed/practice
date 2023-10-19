7d5d60d1acd89435eb09de11fe7033f56eed7907
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-19 20:11:30 +0300

    cbv multiple object mixin detail

--- products_two/views.py
+++ products_two/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import View, ListView, DetailView
 from django.views.generic.list import MultipleObjectMixin
 from django.shortcuts import render
+from django.http import Http404
 
 from .models import Product, DigitalProduct
 from .mixins import ProductTemplateMixin, QuerysetModelMixin
@@ -20,9 +21,24 @@ class ProductListView(ListView):
 
 
 
-class ProductDetailView(ProductTemplateMixin, DetailView):
+class ProductDetailView(DetailView):
   model = Product
   template_name = 'products/product_detail.html'
 
-  def get_title(self):
-    return self.get_object().title
+
+class ProductDetailView(MultipleObjectMixin, View): # https://docs.djangoproject.com/en/4.2/ref/class-based-views/
+  queryset = Product.objects.all()
+  
+  def get(self, request, *args, **kwargs):
+    pk = kwargs.get('pk')
+    self.object_list = self.get_queryset().filter(pk=pk)
+    qs = self.object_list
+    if not qs.exists():
+      raise Http404('This was not found')
+    obj = qs.get()
+    context = self.get_context_data()
+    context['object'] = obj
+    app_label = self.object_list.model._meta.app_label
+    model_name = self.object_list.model._meta.model_name
+    template = f'{app_label}/{model_name}_detail.html'
+    return render(request, template, context)
