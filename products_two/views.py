c2eb222bc3747a630644d0550b94b64718d8a843
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-19 19:38:03 +0300

    cbv list view

--- products_two/views.py
+++ products_two/views.py
@@ -14,16 +14,9 @@ class DigitalProductListView(ProductTemplateMixin, ListView):
 
 
 
-class ProductListView(MultipleObjectMixin, View):
-  queryset = Product.objects.filter(pk__gte=0)
-  
-  def get(self, request, *args, **kwargs):
-    self.object_list = self.get_queryset()
-    context = self.get_context_data()
-    app_label = self.object_list.model._meta.app_label
-    model_name = self.object_list.model._meta.model_name
-    template = f'{app_label}/{model_name}_list.html'
-    return render(request, template, context)
+class ProductListView(ListView):
+  model = Product
+  title = 'Products'
 
 
 
