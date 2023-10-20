4ea0fbca7f86a0e641facd79ad7d7f7742c907f5
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-20 12:44:16 +0300

    model form

--- products_two/models.py
+++ products_two/models.py
@@ -1,9 +1,13 @@
+from django.conf import settings
 from django.db import models
+
+User = settings.AUTH_USER_MODEL
 
 
 class Product(models.Model):
+  user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='products_two')
   title = models.CharField(max_length=120)
-  slug = models.SlugField()
+  slug = models.SlugField(unique=True)
 
 class DigitalProduct(Product):
   class Meta:
