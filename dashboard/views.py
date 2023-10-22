a93e2f52457abed66218bb69ab18289edea3988c
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-22 14:40:31 +0300

    cbv login required method decorator

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,12 @@
 from multiprocessing import context
+from typing import Any
+from django import http
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.base import TemplateResponseMixin, ContextMixin
 from django.views.generic import TemplateView, View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 
@@ -16,6 +20,9 @@ class AboutTemplateView(TemplateView):
   
 class MyView(ContextMixin, TemplateResponseMixin, View):
   template_name = 'dashboard/about.html'
+
+  @method_decorator(login_required)
   def get(self, request, *args, **kwargs):
     context = self.get_context_data(**kwargs)
     return self.render_to_response(context)
+  
