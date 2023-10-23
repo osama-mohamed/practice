f5d9ba5acfdabe4e81487ca101c39862ffa4b1e6
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-23 20:10:09 +0300

    initial form

--- blog_three/forms.py
+++ blog_three/forms.py
@@ -7,6 +7,11 @@ class TestForm(forms.Form):
   integer = forms.IntegerField(initial=1)
   email = forms.EmailField(min_length=6)
 
+  def __init__(self, *args, **kwargs):
+    super(TestForm, self).__init__(*args, **kwargs)
+    self.fields['some_text'].initial = 'Some New Text'
+    self.fields['email'].initial = 'Your e-mail'
+
   def clean_some_text(self, *args, **kwargs):
     some_text = self.cleaned_data.get('some_text')
     if len(some_text) < 10:
