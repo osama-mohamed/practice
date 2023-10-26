a5e5a0187f7defd81473e1aecc1b3a085a310e97
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-26 11:20:30 +0300

    extend custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
 
-from .models import MyUser
+from .models import MyUser, Profile
 
 
 class UserCreationForm(forms.ModelForm):
@@ -84,6 +84,7 @@ class UserAdmin(BaseUserAdmin):
 
 # Now register the new UserAdmin...
 admin.site.register(MyUser, UserAdmin)
+admin.site.register(Profile)
 # ... and, since we're not using Django's built-in permissions,
 # unregister the Group model from admin.
 admin.site.unregister(Group)
