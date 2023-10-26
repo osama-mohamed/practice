c223113c2ea4f672e88f075f8f17c32ece91d3b9
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-26 09:50:26 +0300

    clear out old migrations

--- accounts/models.py
+++ accounts/models.py
@@ -4,23 +4,28 @@ from django.db.models.signals import post_save
 
 from django.conf import settings
 
-
+# https://docs.djangoproject.com/en/4.2/topics/auth/customizing/
 User = settings.AUTH_USER_MODEL
 
-class Profile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  city = models.CharField(max_length=120, null=True, blank=True)
 
-  def __str__(self):
-    return str(self.user.username)
+
+
+
+
+# class Profile(models.Model):
+#   user = models.OneToOneField(User, on_delete=models.CASCADE)
+#   city = models.CharField(max_length=120, null=True, blank=True)
+
+#   def __str__(self):
+#     return str(self.user.username)
   
 
 
-def post_save_user_model_receiver(sender, instance, created, *args, **kwargs):
-  if created:
-    try:
-      Profile.objects.create(user=instance)
-    except:
-      pass
+# def post_save_user_model_receiver(sender, instance, created, *args, **kwargs):
+#   if created:
+#     try:
+#       Profile.objects.create(user=instance)
+#     except:
+#       pass
 
-post_save.connect(post_save_user_model_receiver, sender=User)
+# post_save.connect(post_save_user_model_receiver, sender=User)
