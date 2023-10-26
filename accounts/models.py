045a8f50cdb5c6cc3ca8d39514ce74a8ea792c7d
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-26 11:01:23 +0300

    custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.db.models.signals import post_save
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import (
+  BaseUserManager, 
+  AbstractBaseUser,
+)
 
 
 from django.conf import settings
@@ -9,6 +14,96 @@ User = settings.AUTH_USER_MODEL
 
 
 
+
+class MyUserManager(BaseUserManager):
+  def create_user(self, username, email, password=None):
+    """
+    Creates and saves a User with the given email, date of
+    birth and password.
+    """
+    if not email:
+      raise ValueError("Users must have an email address")
+
+    user = self.model(
+      username=username,
+      email=self.normalize_email(email),
+    )
+
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+
+  def create_superuser(self, username, email, password=None):
+    """
+    Creates and saves a superuser with the given email, date of
+    birth and password.
+    """
+    user = self.create_user(
+      username,
+      email,
+      password=password,
+    )
+    user.is_admin = True
+    user.is_staff = True
+    user.save(using=self._db)
+    return user
+
+
+USERNAME_REGEX = '^[a-zA-Z0-9.-]*$'
+
+
+class MyUser(AbstractBaseUser):
+  username = models.CharField(
+    max_length=255, 
+    validators=[
+      RegexValidator(
+        regex=USERNAME_REGEX,
+        message='Username must be Alpahnumeric or contain any of the following: ". -" ',
+        code='invalid_username'
+      )],
+    unique=True,
+  )
+  email = models.EmailField(
+    verbose_name='email address',
+    max_length=255,
+    unique=True,
+  )
+  zipcode = models.CharField(max_length=120, default="92660")
+  is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
+  is_admin = models.BooleanField(default=False)
+
+  objects = MyUserManager()
+
+  USERNAME_FIELD = 'username'
+  REQUIRED_FIELDS = ['email']
+
+  def get_full_name(self):
+    # The user is identified by their email address
+    return self.email
+
+  def get_short_name(self):
+    # The user is identified by their email address
+    return self.email
+
+  def __str__(self):
+    return self.username
+
+  def has_perm(self, perm, obj=None):
+    "Does the user have a specific permission?"
+    # Simplest possible answer: Yes, always
+    return True
+
+  def has_module_perms(self, app_label):
+    "Does the user have permissions to view the app `app_label`?"
+    # Simplest possible answer: Yes, always
+    return True
+
+  # @property
+  # def is_staff(self):
+  #   "Is the user a member of staff?"
+  #   # Simplest possible answer: All admins are staff
+  #   return self.is_admin
 
 
 
