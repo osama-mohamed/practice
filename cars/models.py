90734bbbabf5c8774e4dd89fd38b3f00d5d510b0
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-28 18:52:04 +0200

    custom on delete

--- cars/models.py
+++ cars/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 User = settings.AUTH_USER_MODEL
 
 
+def set_delete_user():
+  return get_user_model().objects.get_or_create(username='deleted')[0]
+
+
 class Car(models.Model):
+  user = models.ForeignKey(User, on_delete=models.SET(set_delete_user))
   # user = models.ForeignKey(User, on_delete=models.CASCADE)
   # drivers = models.ManyToManyField(User)
-  first_owner = models.OneToOneField(User, on_delete=models.CASCADE)
+  # first_owner = models.OneToOneField(User, on_delete=models.CASCADE)
   name = models.CharField(max_length=120)
 
   def __str__(self):
