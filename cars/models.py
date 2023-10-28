d0237a98a377d100191cbb8c116d45aad19ae801
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-28 18:59:39 +0200

    custom limit choices to

--- cars/models.py
+++ cars/models.py
@@ -10,8 +10,17 @@ def set_delete_user():
   return get_user_model().objects.get_or_create(username='deleted')[0]
 
 
+def limit_car_choices():
+  Q = models.Q
+  return Q(username__icontains='o')
+
 class Car(models.Model):
-  user = models.ForeignKey(User, on_delete=models.SET(set_delete_user))
+  user = models.ForeignKey(
+    User, 
+    on_delete=models.SET(set_delete_user), 
+    limit_choices_to={"is_staff": True},
+    # limit_choices_to = limit_car_choices,
+  )
   # user = models.ForeignKey(User, on_delete=models.CASCADE)
   # drivers = models.ManyToManyField(User)
   # first_owner = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -20,6 +29,8 @@ class Car(models.Model):
   def __str__(self):
     return self.name
   
+
+# https://docs.djangoproject.com/en/4.2/ref/models/fields/#module-django.db.models.fields.related
 # ForeignKey => user_obj.car_set.all() 
 # ManyToMany => user_obj.car_set.all()
 # OneToOne => user_obj.car
