e090133e965745f71a6eb3e9e716e65b518c372c
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-28 18:31:14 +0200

    one to one

--- cars/models.py
+++ cars/models.py
@@ -7,9 +7,13 @@ User = settings.AUTH_USER_MODEL
 
 class Car(models.Model):
   # user = models.ForeignKey(User, on_delete=models.CASCADE)
-  drivers = models.ManyToManyField(User)
+  # drivers = models.ManyToManyField(User)
+  first_owner = models.OneToOneField(User, on_delete=models.CASCADE)
   name = models.CharField(max_length=120)
 
   def __str__(self):
     return self.name
   
+# ForeignKey => user_obj.car_set.all() 
+# ManyToMany => user_obj.car_set.all()
+# OneToOne => user_obj.car
