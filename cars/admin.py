0ac9a32f9798ae029ef0350137ac427ce2ed2505
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-28 19:06:54 +0200

    search & change foreign key field in admin

--- cars/admin.py
+++ cars/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Car
 
-admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin):
+  search_fields = ['user__username', 'user__email', 'name', 'user__id']
+  raw_id_fields = ['user']
+  class Meta:
+    model = Car
+
+admin.site.register(Car, CarAdmin)
