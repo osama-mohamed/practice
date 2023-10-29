28d1dfe736ed863172052ac3b9df4c356dc2a857
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-29 11:05:59 +0200

    stop redis & celery settings

--- djviews/settings.py
+++ djviews/settings.py
@@ -140,70 +140,70 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SHORTCODE_MIN = 25
 
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 
-# REDIS SETTINGS
-REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
-FULL_URL = os.getenv('FULL_URL')
-REDIS_URL = os.getenv('REDIS_URL')
-HOST = os.getenv('HOST')
-PORT = os.getenv('PORT')
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": FULL_URL,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "PASSWORD": REDIS_PASSWORD,
-            "SSL": True,
-        }
-    }
-}
+# # REDIS SETTINGS
+# REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+# FULL_URL = os.getenv('FULL_URL')
+# REDIS_URL = os.getenv('REDIS_URL')
+# HOST = os.getenv('HOST')
+# PORT = os.getenv('PORT')
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": FULL_URL,
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#             "PASSWORD": REDIS_PASSWORD,
+#             "SSL": True,
+#         }
+#     }
+# }
 
-# Test REDIS cache
-from django.core.cache import cache
-cache.set('test_key', 'Redis test_value', timeout=60) # Set a value in the cache
-value = cache.get('test_key') # Retrieve the value
-print(value) # Output should be "Redis test_value"
-
-
-import redis
-def check_redis_connection():
-    try:
-        redis_client = redis.StrictRedis(host=HOST, port=PORT, password=REDIS_PASSWORD)    
-        # Test the connection
-        if redis_client.ping():
-            print('Connected to Redis')
-            return True
-        else:
-            print('Could not connect to Redis')         
-            return False
-    except Exception as e:
-        print(f'An error occurred: {e}')
-check_redis_connection()
+# # Test REDIS cache
+# from django.core.cache import cache
+# cache.set('test_key', 'Redis test_value', timeout=60) # Set a value in the cache
+# value = cache.get('test_key') # Retrieve the value
+# print(value) # Output should be "Redis test_value"
 
 
-# CELERY SETTINGS
-CELERY_BROKER_URL = FULL_URL
-CELERY_RESULT_BACKEND = FULL_URL
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = TIME_ZONE # 'Africa/Cairo'
-CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
-CELERY_IMPORTS = ("billing.tasks",)
+# import redis
+# def check_redis_connection():
+#     try:
+#         redis_client = redis.StrictRedis(host=HOST, port=PORT, password=REDIS_PASSWORD)    
+#         # Test the connection
+#         if redis_client.ping():
+#             print('Connected to Redis')
+#             return True
+#         else:
+#             print('Could not connect to Redis')         
+#             return False
+#     except Exception as e:
+#         print(f'An error occurred: {e}')
+# check_redis_connection()
 
-# check celery connection
-import socket
-def check_port(ip, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((ip, port))
-    if result == 0:
-        print(f"Celery port : {port} is open")
-    else:
-        print(f"Celery port : {port} is not open")
-    # return result
-    sock.close()
-check_port(HOST, int(PORT))
+
+# # CELERY SETTINGS
+# CELERY_BROKER_URL = FULL_URL
+# CELERY_RESULT_BACKEND = FULL_URL
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TIMEZONE = TIME_ZONE # 'Africa/Cairo'
+# CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+# CELERY_IMPORTS = ("billing.tasks",)
+
+# # check celery connection
+# import socket
+# def check_port(ip, port):
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     result = sock.connect_ex((ip, port))
+#     if result == 0:
+#         print(f"Celery port : {port} is open")
+#     else:
+#         print(f"Celery port : {port} is not open")
+#     # return result
+#     sock.close()
+# check_port(HOST, int(PORT))
